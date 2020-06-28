c07edab970668502a166de30484f7e28c40df217
alex gomes <alexandro.gomes@useblu.com.br>
2020-06-28 20:58:57 -0300

    Adding struct sizes

--- OceanDesignSystem/Resources/Sizes/OceanExtensionSize.swift
+++ OceanDesignSystem/Resources/Sizes/OceanExtensionSize.swift
@@ -12,6 +12,16 @@ import UIKit
 extension Ocean {
     
     public struct size {
-        public static let fontSizeXxxs : CGFloat = 60
+        public static let fontSizeXxxs : CGFloat = 12
+        public static let fontSizeXxs : CGFloat = 14
+        public static let fontSizeXs : CGFloat = 16
+        public static let fontSizeSm : CGFloat = 20
+        public static let fontSizeMd : CGFloat = 24
+        public static let fontSizeLg : CGFloat = 32
+        public static let fontSizeXl : CGFloat = 40
+        public static let fontSizeXxl : CGFloat = 48
+        public static let fontSizeXxxl : CGFloat = 64
+        public static let fontSizeDisplay : CGFloat = 80
+        public static let fontSizeGiant : CGFloat = 96
     }
 }
