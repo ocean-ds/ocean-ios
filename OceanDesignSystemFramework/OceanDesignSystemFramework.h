ae2a1090049a4504255e0e4eaba8cd879e083f61
alex gomes <alexandro.gomes@useblu.com.br>
2020-06-28 23:08:19 -0300

    creating target framework

---
+++ OceanDesignSystemFramework/OceanDesignSystemFramework.h
@@ -0,0 +1,19 @@
+//
+//  OceanDesignSystemFramework.h
+//  OceanDesignSystemFramework
+//
+//  Created by Alexandro Gomes on 28/06/20.
+//  Copyright © 2020 Blu Pagamentos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OceanDesignSystemFramework.
+FOUNDATION_EXPORT double OceanDesignSystemFrameworkVersionNumber;
+
+//! Project version string for OceanDesignSystemFramework.
+FOUNDATION_EXPORT const unsigned char OceanDesignSystemFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OceanDesignSystemFramework/PublicHeader.h>
+
+
---
+++ OceanDesignTokens/OceanDesignTokens.h
@@ -0,0 +1,19 @@
+//
+//  OceanDesignTokens.h
+//  OceanDesignTokens
+//
+//  Created by Alexandro Gomes on 28/06/20.
+//  Copyright © 2020 Blu Pagamentos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OceanDesignTokens.
+FOUNDATION_EXPORT double OceanDesignTokensVersionNumber;
+
+//! Project version string for OceanDesignTokens.
+FOUNDATION_EXPORT const unsigned char OceanDesignTokensVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OceanDesignTokens/PublicHeader.h>
+
+
