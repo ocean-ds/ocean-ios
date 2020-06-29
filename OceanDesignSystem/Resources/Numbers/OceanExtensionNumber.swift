e330c4cd0adfa295dad98e8f7b30d697df783850
alex gomes <alexandro.gomes@useblu.com.br>
2020-06-28 21:03:26 -0300

    Adding struct numbers

--- OceanDesignSystem/Resources/Numbers/OceanExtensionNumber.swift
+++ OceanDesignSystem/Resources/Numbers/OceanExtensionNumber.swift
@@ -12,5 +12,14 @@ import UIKit
 extension Ocean {
     struct number {
         static let opacityLevelSemiopaque : CGFloat = 0.8
+        static let opacityLevelIntense : CGFloat = 0.64
+        static let opacityLevelMedium : CGFloat = 0.4
+        static let opacityLevelLight : CGFloat = 0.16
+        static let opacityLevelSemitransparent : CGFloat = 0.08
+        static let fontWeightLight : CGFloat = 300
+        static let fontWeightRegular : CGFloat = 400
+        static let fontWeightMedium : CGFloat = 600
+        static let fontWeightBold : CGFloat = 700
+        static let fontWeightExtrabold : CGFloat = 800
     }
 }
