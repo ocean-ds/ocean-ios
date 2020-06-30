62dea2fe0d23455eb89851d7fd03e92e6a41687d
alex gomes <alexandro.gomes@useblu.com.br>
2020-06-30 20:12:36 -0300

    generate shadow class

--- OceanDesignSystem/Resources/Shadows/OceanExtensionShadow.swift
+++ OceanDesignSystem/Resources/Shadows/OceanExtensionShadow.swift
@@ -1,34 +1,27 @@
-////
-////  OceanExtensionShadow.swift
-////  OceanDesignSystem
-////
-////  Created by Alexandro Gomes on 28/06/20.
-////  Copyright © 2020 Blu Pagamentos. All rights reserved.
-////
-//
-//import Foundation
-//import UIKit
-//
-//extension Ocean {
-//    public struct shadow {
-//        public static let shadowLevel1 : CALayer = OceanShadow.init().boxShadow(0, height: 4, radius: 8, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))!
-//        public static let shadowLevel2 : CALayer = OceanShadow.init().boxShadow(0, height: 8, radius: 16, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))!
-//        public static let shadowLevel3 : CALayer = OceanShadow.init().boxShadow(0, height: 16, radius: 32, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))!
-//        public static let shadowLevel4 : CALayer = OceanShadow.init().boxShadow(0, height: 16, radius: 48, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))!
-//        
-//    }
-//}
-//
-//class OceanShadow  {
-//    
-//    private var customShadow : CALayer?;
-//    
-//    public func boxShadow(_ width: CGFloat, height: CGFloat, radius: CGFloat, color: UIColor) -> CALayer? {
-//        customShadow?.shadowOffset = CGSize(width: width, height: height)
-//        customShadow?.shadowRadius = radius
-//        customShadow?.shadowColor = color.cgColor
-//        
-//        return customShadow;
-//    }
-//    
-//}
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+import UIKit
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - JSON Files
+extension Ocean {
+// swiftlint:disable identifier_name line_length number_separator type_body_length
+public struct shadow {
+  ///public static let shadowLevel1: CALayer = createShadow(x: 0, y: 4, blurRadius: 8, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+  public static let shadowLevel1: CALayer = createShadow(x: 0, y: 4, blurRadius: 8, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+  ///public static let shadowLevel2: CALayer = createShadow(x: 0, y: 8, blurRadius: 16, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+  public static let shadowLevel2: CALayer = createShadow(x: 0, y: 8, blurRadius: 16, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+  ///public static let shadowLevel3: CALayer = createShadow(x: 0, y: 16, blurRadius: 32, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+  public static let shadowLevel3: CALayer = createShadow(x: 0, y: 16, blurRadius: 32, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+  ///public static let shadowLevel4: CALayer = createShadow(x: 0, y: 16, blurRadius: 48, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+  public static let shadowLevel4: CALayer = createShadow(x: 0, y: 16, blurRadius: 48, color: UIColor(red: 12, green: 13, blue: 20, alpha: 0.08))
+}
+  public static func createShadow(x: CGFloat, y: CGFloat, blurRadius: CGFloat, color: UIColor) -> CALayer {
+      return CALayer()
+  }
+}
+// swiftlint:enable identifier_name line_length number_separator type_body_length
