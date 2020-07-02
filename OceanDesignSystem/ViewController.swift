9fea100c41a218df531fc6dbc75d9ec225b0f30f
alex gomes <alexandro.gomes@useblu.com.br>
2020-07-02 14:40:02 -0300

    testing label with fonts registered

--- OceanDesignSystem/ViewController.swift
+++ OceanDesignSystem/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import OceanDesignSystemFramework
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelTest: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        // The interesting stuff
+        let labelShadow = UILabel(frame: CGRect(x: 50, y: 200, width: 120, height: 20))
+        //viewShadow.center = container.center
+        labelShadow.backgroundColor = UIColor.clear
+        labelTest.font = UIFont(name: Ocean.font.fontFamilyBaseWeightLight, size: Ocean.font.fontSizeXs)
+        
+        labelShadow.textColor = UIColor.black
+        labelShadow.applyShadow(parameters: Ocean.shadow.shadowLevel4)
+        self.view.addSubview(labelShadow)
         //self.view.backgroundColor = color.articleBody
         //self.view.backgroundColor = Ocean.articleBody.color
         //self.view.backgroundColor = OceanColors.articleBody
