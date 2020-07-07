6ca13e75fc6bb928ecd980525af00771ca59b3e0
alex gomes <alexandro.gomes@useblu.com.br>
2020-07-07 16:57:25 -0300

    customize shell script with new values

--- generate_classes.sh
+++ generate_classes.sh
@@ -1,21 +1,30 @@
-echo 'Removing all resources in folder Resources'
+
+echo '\033[1;31m Removing all resources in folder Resources…\033[0m'
 rm -fR OceanTokens/Classes/OceanExtensions
 
-echo 'Creating Colors folder'
+echo '\033[1;31m Creating Colors folder…\033[0m'
 mkdir -p OceanTokens/Classes/OceanExtensions/Colors
 
-echo 'Creating Sizes folder'
+echo '\033[1;31m Creating Sizes folder…\033[0m'
 mkdir -p OceanTokens/Classes/OceanExtensions/Sizes
 
-echo 'Creating Shadows folder'
+echo '\033[1;31m Creating Shadows folder…\033[0m'
 mkdir -p OceanTokens/Classes/OceanExtensions/Shadows
 
-echo 'Creating Typography folder'
+echo '\033[1;31m Creating Typography folder…\033[0m'
 mkdir -p OceanTokens/Classes/OceanExtensions/Typography
 
-echo 'Check config swiftgen.yml and run'
-swiftgen config lint && 
-swiftgen config run --verbose && 
+echo '\033[1;31m Checking config swiftgen.yml and run…\033[0m'
+swiftgen config lint --config swiftgen-colors-and-sizes.yml && 
+swiftgen config run --config swiftgen-colors-and-sizes.yml  --verbose && 
+swiftgen config lint --config swiftgen-shadows.yml && 
 swiftgen config run --config swiftgen-shadows.yml  --verbose && 
+swiftgen config lint --config swiftgen-typography.yml  && 
 swiftgen config run --config swiftgen-typography.yml  --verbose && 
+swiftgen config lint --config swiftgen-installer-fonts.yml  && 
 swiftgen config run --config swiftgen-installer-fonts.yml  --verbose
+
+echo '\033[1;31m Copying Fonts ttf to Resources into OceanTokens…\033[0m'
+rm -fR ./OceanTokens/Resources/Fonts
+mkdir -p ./OceanTokens/Resources/Fonts
+cp  -pR  ./node_modules/@useblu/tokens/dist/fonts/* ./OceanTokens/Resources/Fonts
