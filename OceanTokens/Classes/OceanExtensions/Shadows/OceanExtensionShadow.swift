86c7bc12ae0827377b1a201ff7a754f1f9089714
alex gomes <alexandro.gomes@useblu.com.br>
2020-07-07 16:55:16 -0300

    refactoring in tempalte using literal values

--- OceanTokens/Classes/OceanExtensions/Shadows/OceanExtensionShadow.swift
+++ OceanTokens/Classes/OceanExtensions/Shadows/OceanExtensionShadow.swift
@@ -12,21 +12,21 @@ extension Ocean {
 // swiftlint:disable identifier_name line_length number_separator type_body_length
 public struct shadow {
   public typealias ShadowParameters = [String : Any]
-  ///public static let shadowLevel1 : ShadowParameters = ["x": 0, "y": 4, "radius": 8, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
-  public static let shadowLevel1 : ShadowParameters = ["x": 0, "y": 4, "radius": 8, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
-  ///public static let shadowLevel2 : ShadowParameters = ["x": 0, "y": 8, "radius": 16, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
-  public static let shadowLevel2 : ShadowParameters = ["x": 0, "y": 8, "radius": 16, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
-  ///public static let shadowLevel3 : ShadowParameters = ["x": 0, "y": 16, "radius": 32, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
-  public static let shadowLevel3 : ShadowParameters = ["x": 0, "y": 16, "radius": 32, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
-  ///public static let shadowLevel4 : ShadowParameters = ["x": 0, "y": 16, "radius": 48, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
-  public static let shadowLevel4 : ShadowParameters = ["x": 0, "y": 16, "radius": 48, "color": #colorLiteral(red: 12 / 255, green: 13 / 255, blue: 20 / 255, alpha: 0.08)]
+  ///public static let shadowLevel1 : ShadowParameters = ["x": 0, "y": 4, "radius": 8, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
+  public static let shadowLevel1 : ShadowParameters = ["x": 0, "y": 4, "radius": 8, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
+  ///public static let shadowLevel2 : ShadowParameters = ["x": 0, "y": 8, "radius": 16, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
+  public static let shadowLevel2 : ShadowParameters = ["x": 0, "y": 8, "radius": 16, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
+  ///public static let shadowLevel3 : ShadowParameters = ["x": 0, "y": 16, "radius": 32, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
+  public static let shadowLevel3 : ShadowParameters = ["x": 0, "y": 16, "radius": 32, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
+  ///public static let shadowLevel4 : ShadowParameters = ["x": 0, "y": 16, "radius": 48, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
+  public static let shadowLevel4 : ShadowParameters = ["x": 0, "y": 16, "radius": 48, "red": 12,"green": 13,"blue": 20,"alpha": 0.08]
 }
 }
 extension UIView {
   public func applyShadow(parameters: Ocean.shadow.ShadowParameters){
       self.layer.shadowOffset = CGSize(width: parameters["x"] as! Int, height: parameters["y"] as! Int)
       self.layer.shadowRadius = CGFloat(parameters["radius"] as! Int)
-      let color = parameters["color"] as! UIColor
+      let color = UIColor(red: (parameters["red"] as! CGFloat / 255), green: (parameters["green"] as! CGFloat  / 255), blue: (parameters["blue"] as! CGFloat / 255), alpha: parameters["alpha"] as! CGFloat)
       self.layer.shadowColor = color.cgColor
       self.layer.shadowOpacity = 1
   }
