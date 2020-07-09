7a7b3afa6b080a0ae726a4134b664a3270ab72d0
alex gomes <alexandro.gomes@useblu.com.br>
2020-07-09 15:43:33 -0300

    getting latest version token lib

--- publish_pod_ocean_tokens.sh
+++ publish_pod_ocean_tokens.sh
@@ -1,13 +1,14 @@
-
+LATEST_VERSION_TOKENS_LIB_NPM=`npm show  @useblu/tokens version` 
+echo "version:$LATEST_VERSION_TOKENS_LIB_NPM"
 echo "Generating release version number equals npm version…"
-git tag -a "OceanTokens-$1" -m "Release of version $1"
+git tag -a "OceanTokens-$LATEST_VERSION_TOKENS_LIB_NPM" -m "Release of version $LATEST_VERSION_TOKENS_LIB_NPM"
 git push --tags
 
 echo "Creating podspec OceanTokens…"
 cp -fR OceanTokens.template OceanTokens.podspec 
 
 echo "Putting version number equals npm version…"
-sed -i '' "s/#NPM_VERSION#/'$1'/g" OceanTokens.podspec
+sed -i '' "s/#NPM_VERSION#/'$LATEST_VERSION_TOKENS_LIB_NPM'/g" OceanTokens.podspec
 
 echo "Creating new pod…"
 pod trunk push OceanTokens.podspec  
