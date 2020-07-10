85c28fce85042f3739050d249e1a0bac551442d4
alex gomes <alexandro.gomes@useblu.com.br>
2020-07-10 19:15:05 -0300

    Generating classes based npm lib version: 1.0.0

--- generate_classes.sh
+++ generate_classes.sh
@@ -1,5 +1,5 @@
 echo '\033[1;31m Downloading tokens into npm lib…\033[0m'
-yarn add -D @useblu/tokens
+yarn add -D @useblu/tokens@2.0.0-alpha.1
 
 echo '\033[1;31m Removing all resources in folder Resources…\033[0m'
 rm -fR OceanTokens/Classes/OceanExtensions
