bc855ac5371083ff2c3623e189531bb13a2180be
alex gomes <alexandro.gomes@useblu.com.br>
2020-07-10 19:37:06 -0300

    Generating classes based npm lib version: 1.0.0

--- publish_pod_ocean_tokens.sh
+++ publish_pod_ocean_tokens.sh
@@ -6,6 +6,7 @@ sh generate_classes.sh
 
 echo "Add and Commit new classes"
 git add . && git commit -am "Generating classes based npm lib version: $LATEST_VERSION_TOKENS_LIB_NPM"
+git push origin master
 
 echo "Generating release version number equals npm version…"
 git tag -a "OceanTokens-$LATEST_VERSION_TOKENS_LIB_NPM" -m "Release of version $LATEST_VERSION_TOKENS_LIB_NPM"
