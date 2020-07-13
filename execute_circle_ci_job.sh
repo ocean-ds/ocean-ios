e7d17837d16f3151e7b6f3127925e6d6b957abf7
alex gomes <alexandro.gomes@useblu.com.br>
2020-07-13 17:54:02 -0300

    create shell script circle-ci

---
+++ execute_circle_ci_job.sh
@@ -0,0 +1,3 @@
+curl -u 19ec0bbdfaa269ad951e8ee9b21b8b9f960314b3: \
+-d build_parameters\[CIRCLE_JOB\]=deploy-ocean-tokens \
+https://circleci.com/api/v1.1/project/gh/Pagnet/ocean-ds-ios/tree/circleci-project-setup
