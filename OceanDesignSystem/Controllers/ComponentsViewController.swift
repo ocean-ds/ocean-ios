d3e752228f392b808d67862a310dda8b2ace24e8
Alex Gomes <alexandro.gomes@useblu.com.br>
2020-07-23 20:55:32 -0300

    adding heading components for tests

--- OceanDesignSystem/Controllers/ComponentsViewController.swift
+++ OceanDesignSystem/Controllers/ComponentsViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 import OceanTokens
+import OceanComponents
 
 class ComponentsViewController: UITableViewController {
 
-    //@IBOutlet weak var labelTest: UILabel!
+    //var componentsTypeSelected : CategoryType!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let heading1 = Heading { component in
+//            component.itemTitle = "teste"
+//            component.add(view: Spacer(space: Ocean.size.spacingInsetMd))
+//        }
+//        self.add(view: heading1 )
+//        let heading1 = TitleDescription { component in
+//            component.itemTitle = title
+//            component.add(view: Spacer(space: Ocean.size.spacingInsetMd))
+//        }
+        //self.add(view: titleDescripition )
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ComponentsCell", for: indexPath) as! StandardCell
+        let components = Components.list
+        cell.title.text = components[indexPath.row]
+    
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Components.list.count
+    }
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        //self.categoryTypeSelected = CategoryType.init(rawValue: indexPath.row)
+        return indexPath
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
         
     }
 
---
+++ OceanDesignSystem/Models/Components.swift
@@ -0,0 +1,24 @@
+//
+//  Components.swift
+//  OceanDesignSystem
+//
+//  Created by Alex Gomes on 23/07/20.
+//  Copyright © 2020 Blu Pagamentos. All rights reserved.
+//
+
+import Foundation
+
+struct Components {
+    static let list = [
+        "Headings"
+    ]
+}
+
+struct Headings {
+    static let list = [
+        "Heading1",
+        "Heading2",
+        "Heading3",
+        "Heading4"
+    ]
+}
