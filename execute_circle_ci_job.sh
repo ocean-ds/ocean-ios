a7ecd8fa244e97445b521be4245a8fb406b6201a
vromeiro <vinicius.romeiro@gmail.com>
2021-06-23 13:27:24 -0300

    feat: add cd init

--- execute_circle_ci_job.sh
+++
@@ -1,3 +0,0 @@
-curl -u 19ec0bbdfaa269ad951e8ee9b21b8b9f960314b3: \
--d build_parameters\[CIRCLE_JOB\]=deploy-ocean-tokens \
-https://circleci.com/api/v1.1/project/gh/ocean-ds/ocean-ios/tree/circleci-project-setup
