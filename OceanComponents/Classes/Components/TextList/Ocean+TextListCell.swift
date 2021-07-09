1865fceefac3893ea1a00f729692a222688f6dd2
vromeiro <vinicius.romeiro@gmail.com>
2021-07-09 15:12:08 -0300

    feat: add component text list

---
+++ OceanComponents/Classes/Components/TextList/Ocean+TextListCell.swift
@@ -0,0 +1,200 @@
+//
+//  Ocean+TextListCell.swift
+//  OceanComponents
+//
+//  Created by Vini on 09/07/21.
+//
+
+import Foundation
+import OceanTokens
+import UIKit
+
+extension Ocean {
+    public class TextListCell: UIView {
+        struct Constants {
+            static let roundedViewHeightWidth: CGFloat = 40
+        }
+        
+        public var title: String = "" {
+            didSet {
+                updateUI()
+            }
+        }
+        
+        public var subtitle: String = "" {
+            didSet {
+                updateUI()
+            }
+        }
+        
+        public var text: String = "" {
+            didSet {
+                updateUI()
+            }
+        }
+        
+        public var image: UIImage? = nil {
+            didSet {
+                updateUI()
+            }
+        }
+        
+        public var arrow: Bool = false {
+            didSet {
+                updateUI()
+            }
+        }
+        
+        public var badge: Bool = false {
+            didSet {
+                updateUI()
+            }
+        }
+        
+        public var onTouch: (() -> Void)?
+        
+        lazy var mainStack: UIStackView = {
+            UIStackView { stack in
+                stack.axis = .vertical
+                stack.distribution = .fill
+                stack.translatesAutoresizingMaskIntoConstraints = false
+                
+                stack.add([
+                    Ocean.Spacer(space: Ocean.size.spacingStackXs),
+                    contentStack,
+                    Ocean.Spacer(space: Ocean.size.spacingStackXs)
+                ])
+            }
+        }()
+        
+        lazy var contentStack: UIStackView = {
+            UIStackView { stack in
+                stack.axis = .horizontal
+                stack.distribution = .fill
+                stack.alignment = .center
+                stack.translatesAutoresizingMaskIntoConstraints = false
+                
+                stack.add([
+                    Ocean.Spacer(space: Ocean.size.spacingStackXs),
+                    roundedIconView,
+                    Ocean.Spacer(space: Ocean.size.spacingStackXs),
+                    infoStack,
+                    Ocean.Spacer(space: Ocean.size.spacingStackXs),
+                    arrowImageView,
+                    Ocean.Spacer(space: Ocean.size.spacingStackXs)
+                ])
+            }
+        }()
+        
+        lazy var roundedIconView: UIView = {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.clipsToBounds = true
+            view.layer.cornerRadius = Constants.roundedViewHeightWidth / 2
+            view.backgroundColor = Ocean.color.colorInterfaceLightUp
+            view.addSubview(typeImageView)
+            
+            NSLayoutConstraint.activate([
+                view.heightAnchor.constraint(equalToConstant: Constants.roundedViewHeightWidth),
+                view.widthAnchor.constraint(equalToConstant: Constants.roundedViewHeightWidth),
+                typeImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                typeImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+            
+            return view
+        }()
+        
+        lazy var typeImageView: UIImageView = {
+            let view = UIImageView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
+        
+        lazy var arrowImageView: UIImageView = {
+            let view = UIImageView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.image = Ocean.icon.chevronRightSolid
+            view.contentMode = .scaleAspectFit
+            return view
+        }()
+        
+        lazy var badgeView = Ocean.Badge()
+        
+        lazy var infoStackTitle: UIStackView = {
+            UIStackView { stack in
+                stack.translatesAutoresizingMaskIntoConstraints = false
+                stack.axis = .horizontal
+                stack.distribution = .fillProportionally
+                stack.alignment = .leading
+                
+                stack.add([
+                    titleLabel,
+                    Ocean.Spacer(space: Ocean.size.spacingStackXxs),
+                    badgeView
+                ])
+            }
+        }()
+        
+        lazy var infoStack: UIStackView = {
+            UIStackView { stack in
+                stack.translatesAutoresizingMaskIntoConstraints = false
+                stack.axis = .vertical
+                stack.distribution = .fill
+                stack.alignment = .leading
+                
+                stack.add([
+                    infoStackTitle,
+                    subtitleLabel,
+                    textLabel
+                ])
+            }
+        }()
+        
+        lazy var titleLabel: UILabel = {
+            Ocean.Typography.paragraph { label in
+                label.font = .baseSemiBold(size: Ocean.font.fontSizeXs)
+                label.textColor = Ocean.color.colorInterfaceDarkDeep
+            }
+        }()
+        
+        lazy var subtitleLabel = Ocean.Typography.description()
+
+        lazy var textLabel = Ocean.Typography.caption()
+        
+        public convenience init(builder: TextListCellBuilder) {
+            self.init()
+            builder(self)
+        }
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupUI()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        private func setupUI() {
+            self.add(view: mainStack)
+            
+            self.addTapGesture(selector: #selector(viewTapped))
+        }
+
+        func updateUI() {
+            titleLabel.text = title
+            subtitleLabel.isHidden = subtitle.isEmpty
+            subtitleLabel.text = subtitle
+            textLabel.isHidden = text.isEmpty
+            textLabel.text = text
+            typeImageView.image = image
+            roundedIconView.isHidden = image == nil
+            arrowImageView.isHidden = !arrow
+            badgeView.isHidden = !badge
+        }
+        
+        @objc func viewTapped() {
+            self.onTouch?()
+        }
+    }
+}
---
+++ OceanComponents/Classes/UIKit/Extensions/UIVIew+Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  UIVIew+Extensions.swift
+//  OceanComponents
+//
+//  Created by Vini on 09/07/21.
+//
+
+import UIKit
+
+extension UIView {
+    public func add(view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(view)
+        
+        if #available(iOS 11.0, *) {
+            NSLayoutConstraint.activate([
+                view.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+                view.leadingAnchor.constraint(equalTo: leadingAnchor),
+                view.trailingAnchor.constraint(equalTo: trailingAnchor),
+                view.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+            ])
+        } else {
+            NSLayoutConstraint.activate([
+                view.topAnchor.constraint(equalTo: topAnchor),
+                view.leadingAnchor.constraint(equalTo: leadingAnchor),
+                view.trailingAnchor.constraint(equalTo: trailingAnchor),
+                view.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+        }
+    }
+    
+    public func addTapGesture(selector: Selector) {
+        let tap = UITapGestureRecognizer(target: self, action: selector)
+        self.addGestureRecognizer(tap)
+        self.isUserInteractionEnabled = true
+    }
+}
