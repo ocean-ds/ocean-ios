7ea5ed80122d6975df1abd6ec7a5dfd1c5ef53bd
leticia-fernandes-blu <95654372+leticia-fernandes-blu@users.noreply.github.com>
2021-12-27 17:45:59 -0300

    feat: add id property in Ocean+ChipModel (#100)

--- OceanComponents/Classes/Components/Chips/Ocean+ChipModel.swift
+++ OceanComponents/Classes/Components/Chips/Ocean+ChipModel.swift
@@ -9,13 +9,20 @@ import OceanTokens
 
 extension Ocean {
     public struct ChipModel {
+        public let id: String?
         public let icon: UIImage?
         public let number: Int?
         public let title: String
         public let type: Ocean.ChipType
         public var status: Ocean.ChipStatus
         
-        public init(icon: UIImage? = nil, number: Int? = nil, title: String, type: Ocean.ChipType = .choice, status: Ocean.ChipStatus = .normal) {
+        public init(id: String? = nil,
+                    icon: UIImage? = nil,
+                    number: Int? = nil,
+                    title: String,
+                    type: Ocean.ChipType = .choice,
+                    status: Ocean.ChipStatus = .normal) {
+            self.id = id
             self.icon = icon
             self.number = number
             self.title = title
