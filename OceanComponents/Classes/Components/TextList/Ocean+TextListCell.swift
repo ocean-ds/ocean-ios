68774988a048b4a380f6b7904243ee42d9f6ec8d
leticia-fernandes-blu <95654372+leticia-fernandes-blu@users.noreply.github.com>
2022-01-10 16:40:25 -0300

    feat: include option to add a button at TextListItem component (#103)
    * feat: include option to add a button at TextListItem Component
    
    * feat: add example TextListItem with button
    
    * fix: code review changes
    
    * fix: rename button

--- OceanComponents/Classes/Components/TextList/Ocean+TextListCell.swift
+++ OceanComponents/Classes/Components/TextList/Ocean+TextListCell.swift
@@ -16,62 +16,74 @@ extension Ocean {
         case inverse
         case inverseHighlight
     }
-    
+
     public class TextListCell: UIView {
         struct Constants {
             static let roundedViewHeightWidth: CGFloat = 40
         }
-        
+
         public var isInverse: Bool = false
-        
+
         public var title: String = "" {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var subtitle: String = "" {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var text: String = "" {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var image: UIImage? = nil {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var arrow: Bool = false {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var badge: Bool = false {
             didSet {
                 updateUI()
             }
         }
-        
+
+        public var buttonTitle: String? = nil {
+            didSet {
+                updateUI()
+            }
+        }
+
+        public var onTouchButton: (() -> Void)? {
+            didSet {
+                updateUI()
+            }
+        }
+
         public var onTouch: (() -> Void)?
-        
+
         private lazy var mainStack: UIStackView = {
             UIStackView { stack in
                 stack.axis = .vertical
                 stack.distribution = .fill
                 stack.translatesAutoresizingMaskIntoConstraints = false
-                
+
                 stack.add([
                     contentStack
                 ])
-                
+
                 stack.isLayoutMarginsRelativeArrangement = true
                 stack.layoutMargins = .init(top: Ocean.size.spacingStackXs,
                                             left: 0,
@@ -79,14 +91,14 @@ extension Ocean {
                                             right: 0)
             }
         }()
-        
+
         private lazy var contentStack: UIStackView = {
             UIStackView { stack in
                 stack.axis = .horizontal
                 stack.distribution = .fill
                 stack.alignment = .center
                 stack.translatesAutoresizingMaskIntoConstraints = false
-                
+
                 stack.add([
                     Ocean.Spacer(space: Ocean.size.spacingStackXs),
                     roundedIconView,
@@ -94,13 +106,14 @@ extension Ocean {
                     infoStack,
                     arrowImageViewSpacer,
                     arrowImageView,
+                    button,
                     Ocean.Spacer(space: Ocean.size.spacingStackXs)
                 ])
             }
         }()
-        
+
         private lazy var roundedIconViewSpacer = Ocean.Spacer(space: Ocean.size.spacingStackXs)
-        
+
         private lazy var roundedIconView: UIView = {
             let view = UIView()
             view.translatesAutoresizingMaskIntoConstraints = false
@@ -108,26 +121,26 @@ extension Ocean {
             view.layer.cornerRadius = Constants.roundedViewHeightWidth / 2
             view.backgroundColor = Ocean.color.colorInterfaceLightUp
             view.addSubview(iconView)
-            
+
             NSLayoutConstraint.activate([
                 view.heightAnchor.constraint(equalToConstant: Constants.roundedViewHeightWidth),
                 view.widthAnchor.constraint(equalToConstant: Constants.roundedViewHeightWidth),
                 iconView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
                 iconView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
             ])
-            
+
             return view
         }()
-        
+
         private lazy var iconView: UIImageView = {
             let view = UIImageView()
             view.translatesAutoresizingMaskIntoConstraints = false
             view.tintColor = Ocean.color.colorBrandPrimaryDown
             return view
         }()
-        
+
         private lazy var arrowImageViewSpacer = Ocean.Spacer(space: Ocean.size.spacingStackXs)
-        
+
         private lazy var arrowImageView: UIImageView = {
             let view = UIImageView()
             view.translatesAutoresizingMaskIntoConstraints = false
@@ -135,16 +148,16 @@ extension Ocean {
             view.contentMode = .scaleAspectFit
             return view
         }()
-        
+
         private lazy var badgeView = Ocean.Badge.text()
-        
+
         private lazy var infoStackTitle: UIStackView = {
             UIStackView { stack in
                 stack.translatesAutoresizingMaskIntoConstraints = false
                 stack.axis = .horizontal
                 stack.distribution = .fill
                 stack.alignment = .leading
-                
+
                 stack.add([
                     titleLabel,
                     Ocean.Spacer(space: Ocean.size.spacingStackXxs),
@@ -152,14 +165,14 @@ extension Ocean {
                 ])
             }
         }()
-        
+
         private lazy var infoStack: UIStackView = {
             UIStackView { stack in
                 stack.translatesAutoresizingMaskIntoConstraints = false
                 stack.axis = .vertical
                 stack.distribution = .fill
                 stack.alignment = .leading
-                
+
                 stack.add([
                     infoStackTitle,
                     subtitleLabel,
@@ -167,7 +180,11 @@ extension Ocean {
                 ])
             }
         }()
-        
+
+        private lazy var button: UIButton = {
+            UIButton(frame: .zero)
+        }()
+
         private lazy var titleLabel: UILabel = {
             Ocean.Typography.paragraph { label in
                 label.font = .baseSemiBold(size: Ocean.font.fontSizeXs)
@@ -175,7 +192,7 @@ extension Ocean {
                 label.numberOfLines = 1
             }
         }()
-        
+
         private lazy var subtitleLabel: UILabel = {
             Ocean.Typography.description { label in
                 label.numberOfLines = 1
@@ -187,7 +204,7 @@ extension Ocean {
                 label.numberOfLines = 1
             }
         }()
-        
+
         public convenience init(type: TextListType = .normal, builder: TextListCellBuilder = nil) {
             self.init()
             if type == .inverse {
@@ -197,16 +214,16 @@ extension Ocean {
             }
             builder?(self)
         }
-        
+
         override init(frame: CGRect) {
             super.init(frame: frame)
             setupUI()
         }
-        
+
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
-        
+
         private func setupInverse() {
             self.isInverse = true
             self.titleLabel.font = .baseRegular(size: Ocean.font.fontSizeXxs)
@@ -215,7 +232,7 @@ extension Ocean {
             self.subtitleLabel.textColor = Ocean.color.colorInterfaceDarkDeep
             self.subtitleLabel.numberOfLines = 0
         }
-        
+
         private func setupInverseHighlight() {
             self.isInverse = true
             self.titleLabel.font = .baseRegular(size: Ocean.font.fontSizeXxs)
@@ -225,16 +242,16 @@ extension Ocean {
             self.subtitleLabel.setLineHeight(lineHeight: Ocean.font.lineHeightComfy)
             self.subtitleLabel.numberOfLines = 0
         }
-        
+
         private func setupUI() {
             self.add(view: mainStack)
-            
+
             self.addTapGesture(selector: #selector(viewTapped))
         }
 
         private func updateUI() {
             let imageNotExist = image == nil
-            
+
             titleLabel.text = title
             subtitleLabel.isHidden = subtitle.isEmpty
             subtitleLabel.text = subtitle
@@ -246,7 +263,12 @@ extension Ocean {
             arrowImageViewSpacer.isHidden = !arrow
             arrowImageView.isHidden = !arrow
             badgeView.isHidden = !badge
-            
+            button.isHidden = true
+
+            if let title = buttonTitle {
+                setupButton(title: title)
+            }
+
             if isInverse {
                 if imageNotExist {
                     mainStack.layoutMargins = .init(top: Ocean.size.spacingStackXxs,
@@ -261,7 +283,20 @@ extension Ocean {
                 }
             }
         }
-        
+
+        private func setupButton(title: String) {
+            button.isHidden = false
+            button.setTitle(title, for: .normal)
+            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+
+            button.titleLabel?.font = UIFont(name: Ocean.font.fontFamilyBaseWeightRegular, size: Ocean.font.fontSizeXxs)
+            button.setTitleColor(Ocean.color.colorBrandPrimaryPure, for: .normal)
+            button.contentEdgeInsets = UIEdgeInsets(top: Ocean.size.spacingInsetXs, left: Ocean.size.spacingInsetSm, bottom: Ocean.size.spacingInsetXs, right: Ocean.size.spacingInsetSm)
+            button.titleLabel?.textAlignment = .center
+            button.titleLabel?.adjustsFontSizeToFitWidth = true
+            button.titleLabel?.minimumScaleFactor = 0.6
+        }
+
         public func setSkeleton() {
             self.isSkeletonable = true
             self.mainStack.isSkeletonable = true
@@ -274,16 +309,20 @@ extension Ocean {
             self.subtitleLabel.isSkeletonable = true
             self.textLabel.isSkeletonable = true
         }
-        
+
         public func setSkeletonInverse() {
             self.isSkeletonable = true
             self.mainStack.isSkeletonable = true
             self.contentStack.isSkeletonable = true
             self.infoStack.isSkeletonable = true
         }
-        
+
         @objc func viewTapped() {
             self.onTouch?()
+        }
+
+        @objc private func buttonTapped() {
+            self.onTouchButton?()
         }
     }
 }
