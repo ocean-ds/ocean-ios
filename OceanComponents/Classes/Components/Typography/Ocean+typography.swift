a3b1e53bf824e1990cb809c81586ee8263822baa
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2022-08-17 16:23:53 -0300

    feat: add heading 5

--- OceanComponents/Classes/Components/Typography/Ocean+typography.swift
+++ OceanComponents/Classes/Components/Typography/Ocean+typography.swift
@@ -17,7 +17,7 @@ extension Ocean {
         public static func heading1(builder: TypographyBuilder = nil) -> UILabel {
             return UILabel { label in
                 label.font = .highlightExtraBold(size: Ocean.font.fontSizeLg)
-                label.textColor = Ocean.color.colorInterfaceDarkPure
+                label.textColor = Ocean.color.colorInterfaceDarkDeep
                 label.setLineHeight(lineHeight: Ocean.font.lineHeightMedium)
                 builder?( label )
             }
@@ -26,7 +26,7 @@ extension Ocean {
         public static func heading2(builder: TypographyBuilder = nil) -> UILabel {
             return UILabel { label in
                 label.font = .highlightExtraBold(size: Ocean.font.fontSizeMd)
-                label.textColor = Ocean.color.colorInterfaceDarkPure
+                label.textColor = Ocean.color.colorInterfaceDarkDeep
                 label.setLineHeight(lineHeight: Ocean.font.lineHeightMedium)
                 builder?( label )
             }
@@ -35,7 +35,7 @@ extension Ocean {
         public static func heading3(builder: TypographyBuilder = nil) -> UILabel {
             return UILabel { label in
                 label.font = .highlightExtraBold(size: Ocean.font.fontSizeSm)
-                label.textColor = Ocean.color.colorInterfaceDarkPure
+                label.textColor = Ocean.color.colorInterfaceDarkDeep
                 label.setLineHeight(lineHeight: Ocean.font.lineHeightMedium)
                 builder?( label )
             }
@@ -44,7 +44,16 @@ extension Ocean {
         public static func heading4(builder: TypographyBuilder = nil) -> UILabel {
             return UILabel { label in
                 label.font = .highlightBold(size: Ocean.font.fontSizeXs)
-                label.textColor = Ocean.color.colorInterfaceDarkPure
+                label.textColor = Ocean.color.colorInterfaceDarkDeep
+                label.setLineHeight(lineHeight: Ocean.font.lineHeightMedium)
+                builder?( label )
+            }
+        }
+
+        public static func heading5(builder: TypographyBuilder = nil) -> UILabel {
+            return UILabel { label in
+                label.font = .highlightBold(size: Ocean.font.fontSizeXxs)
+                label.textColor = Ocean.color.colorInterfaceDarkDeep
                 label.setLineHeight(lineHeight: Ocean.font.lineHeightMedium)
                 builder?( label )
             }
@@ -76,7 +85,13 @@ extension Ocean {
                 builder?(label)
                 label.textColor = Ocean.color.colorInterfaceLightPure
             }
-            
+        }
+
+        public static func heading5Inverse(builder: TypographyBuilder = nil) -> UILabel {
+            return Ocean.Typography.heading5 { label in
+                builder?(label)
+                label.textColor = Ocean.color.colorInterfaceLightPure
+            }
         }
         
         public static func subTitle1(builder: TypographyBuilder = nil) -> UILabel {
