265fb865ba677fab9346db77f8367ba08bdd6d7b
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2022-08-23 13:39:24 -0300

    fix: to ocean status tag set nullable

--- OceanComponents/Classes/UIKit/Extensions/String+TagExtensions.swift
+++ OceanComponents/Classes/UIKit/Extensions/String+TagExtensions.swift
@@ -9,13 +9,14 @@ import Foundation
 import OceanTokens
 
 public extension String {
-    func toOceanTagStatus() -> Ocean.Tag.Status {
+    func toOceanTagStatus() -> Ocean.Tag.Status? {
         switch self.lowercased() {
         case "positive": return Ocean.Tag.Status.positive
         case "warning": return Ocean.Tag.Status.warning
         case "negative": return Ocean.Tag.Status.negative
         case "neutral2": return Ocean.Tag.Status.neutral2
-        default: return Ocean.Tag.Status.neutral1
+        case "neutral1": return Ocean.Tag.Status.neutral1
+        default: return nil
         }
     }
 }
