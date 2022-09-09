df0a89758171bbd0ec0363784d5082bd2f780cab
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2022-09-09 11:48:24 -0300

    feat: add tag image status

--- OceanComponents/Classes/Components/Tag/Ocean+Tag.swift
+++ OceanComponents/Classes/Components/Tag/Ocean+Tag.swift
@@ -13,9 +13,9 @@ extension Ocean {
             static let height: CGFloat = 20
             static let imageSize: CGFloat = 16
         }
-        
+
         public typealias TagBuilder = (Tag) -> Void
-        
+
         public enum Status {
             case positive
             case warning
@@ -23,39 +23,45 @@ extension Ocean {
             case neutral1
             case neutral2
         }
-        
+
         public var status: Status = .neutral1 {
             didSet {
                 updateStatus()
             }
         }
-        
+
         public var image: UIImage? {
             didSet {
                 updateUI()
             }
         }
-        
+
+        public var imageStatus: Bool = false {
+            didSet {
+                updateStatus()
+            }
+        }
+
         public var title: String = "" {
             didSet {
                 updateUI()
             }
         }
-        
+
         private lazy var imageView: UIImageView = {
             UIImageView { view in
                 view.contentMode = .scaleAspectFit
                 view.tintColor = Ocean.color.colorInterfaceDarkUp
                 view.isHidden = true
                 view.translatesAutoresizingMaskIntoConstraints = false
-                
+
                 NSLayoutConstraint.activate([
                     view.widthAnchor.constraint(equalToConstant: Constants.imageSize),
                     view.heightAnchor.constraint(equalToConstant: Constants.imageSize)
                 ])
             }
         }()
-        
+
         private lazy var titleLabel: UILabel = {
             UILabel { label in
                 label.font = .baseSemiBold(size: Ocean.font.fontSizeXxxs)
@@ -64,9 +70,9 @@ extension Ocean {
                 label.translatesAutoresizingMaskIntoConstraints = false
             }
         }()
-        
+
         private lazy var imageSpacer = Ocean.Spacer(space: Ocean.size.spacingStackXxxs)
-        
+
         private lazy var mainStack: Ocean.StackView = {
             let stack = Ocean.StackView()
             stack.axis = .horizontal
@@ -74,7 +80,7 @@ extension Ocean {
             stack.alignment = .center
             stack.spacing = 0
             stack.translatesAutoresizingMaskIntoConstraints = false
-            
+
             stack.add([
                 Ocean.Spacer(space: Ocean.size.spacingStackXxxs),
                 imageView,
@@ -83,57 +89,62 @@ extension Ocean {
                 Ocean.Spacer(space: Ocean.size.spacingStackXxxs),
                 imageSpacer
             ])
-            
+
             return stack
         }()
-        
+
         public convenience init(builder: TagBuilder) {
             self.init()
             setupUI()
             builder(self)
         }
-        
+
         private func setupUI() {
             self.translatesAutoresizingMaskIntoConstraints = false
             self.layer.cornerRadius = Constants.height * Ocean.size.borderRadiusCircular
             self.backgroundColor = Ocean.color.colorInterfaceLightUp
             self.add(view: mainStack)
-            
+
             self.heightAnchor.constraint(equalToConstant: Constants.height).isActive = true
         }
-        
+
         private func updateUI() {
             imageView.image = image?.withRenderingMode(.alwaysTemplate)
             imageView.isHidden = image == nil
             imageSpacer.isHidden = image != nil
             titleLabel.text = title
         }
-        
+
         private func updateStatus() {
             switch self.status {
             case .positive:
                 self.backgroundColor = Ocean.color.colorStatusPositiveUp
                 self.imageView.tintColor = Ocean.color.colorStatusPositiveDeep
                 self.titleLabel.textColor = Ocean.color.colorStatusPositiveDeep
+                self.imageView.image = imageStatus ? Ocean.icon.checkCircleSolid?.withRenderingMode(.alwaysTemplate) : nil
             case .warning:
                 self.backgroundColor = Ocean.color.colorStatusNeutralUp
                 self.imageView.tintColor = Ocean.color.colorStatusNeutralDeep
                 self.titleLabel.textColor = Ocean.color.colorStatusNeutralDeep
+                self.imageView.image = imageStatus ? Ocean.icon.exclamationCircleSolid?.withRenderingMode(.alwaysTemplate) : nil
             case .negative:
                 self.backgroundColor = Ocean.color.colorStatusNegativeUp
                 self.imageView.tintColor = Ocean.color.colorStatusNegativePure
                 self.titleLabel.textColor = Ocean.color.colorStatusNegativePure
+                self.imageView.image = imageStatus ? Ocean.icon.xCircleSolid?.withRenderingMode(.alwaysTemplate) : nil
             case .neutral2:
                 self.backgroundColor = Ocean.color.colorInterfaceLightUp
                 self.imageView.tintColor = Ocean.color.colorBrandPrimaryDown
                 self.titleLabel.textColor = Ocean.color.colorBrandPrimaryDown
+                self.imageView.image = nil
             default:
                 self.backgroundColor = Ocean.color.colorInterfaceLightUp
                 self.imageView.tintColor = Ocean.color.colorInterfaceDarkUp
                 self.titleLabel.textColor = Ocean.color.colorInterfaceDarkUp
+                self.imageView.image = nil
             }
         }
-        
+
         public func setSkeleton() {
             self.isSkeletonable = true
         }
