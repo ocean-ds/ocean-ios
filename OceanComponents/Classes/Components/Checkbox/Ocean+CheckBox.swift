00f6f95495fd758560f935c8c535118da58ec149
AcassioM <94627996+AcassioM@users.noreply.github.com>
2023-02-13 16:34:04 -0300

    feat: update constraints off component

--- OceanComponents/Classes/Components/Checkbox/Ocean+CheckBox.swift
+++ OceanComponents/Classes/Components/Checkbox/Ocean+CheckBox.swift
@@ -10,6 +10,9 @@ import UIKit
 
 extension Ocean {
     public class CheckBox: RadioButton {
+        
+       
+        
         override var withAnimation: Bool {
             get {
                 return false
@@ -21,24 +24,10 @@ extension Ocean {
                 return UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: size, height: size), cornerRadius: Ocean.size.borderRadiusSm).cgPath
             }
         }
-        
+
         override var foregroundShrinkPath: CGPath {
             get {
-                let bezierPath = UIBezierPath()
-                bezierPath.move(to: CGPoint(x: 6.24, y: 11.11))
-                bezierPath.addLine(to: CGPoint(x: 11.44, y: 16.83))
-                bezierPath.addLine(to: CGPoint(x: 9.91, y: 16.88))
-                bezierPath.addLine(to: CGPoint(x: 17.71, y: 6.88))
-                bezierPath.addCurve(to: CGPoint(x: 19.12, y: 6.71), controlPoint1: CGPoint(x: 18.05, y: 6.45), controlPoint2: CGPoint(x: 18.68, y: 6.37))
-                bezierPath.addCurve(to: CGPoint(x: 19.29, y: 8.12), controlPoint1: CGPoint(x: 19.55, y: 7.05), controlPoint2: CGPoint(x: 19.63, y: 7.68))
-                bezierPath.addLine(to: CGPoint(x: 11.49, y: 18.12))
-                bezierPath.addCurve(to: CGPoint(x: 9.96, y: 18.17), controlPoint1: CGPoint(x: 11.11, y: 18.6), controlPoint2: CGPoint(x: 10.38, y: 18.63))
-                bezierPath.addLine(to: CGPoint(x: 4.76, y: 12.46))
-                bezierPath.addCurve(to: CGPoint(x: 4.83, y: 11.05), controlPoint1: CGPoint(x: 4.39, y: 12.05), controlPoint2: CGPoint(x: 4.42, y: 11.42))
-                bezierPath.addCurve(to: CGPoint(x: 6.24, y: 11.11), controlPoint1: CGPoint(x: 5.24, y: 10.67), controlPoint2: CGPoint(x: 5.87, y: 10.7))
-                bezierPath.close()
-                bezierPath.fill()
-                return bezierPath.cgPath
+                self.setupCheckIcon()
             }
         }
         
@@ -54,6 +43,26 @@ extension Ocean {
             isSelected = !isSelected
             onTouch?()
             generator.selectionChanged()
+        }
+        
+        private func setupCheckIcon() -> CGPath {
+            let bezierPath = UIBezierPath()
+            bezierPath.move(to: CGPoint(x: 5, y: 10.18))
+            bezierPath.addLine(to: CGPoint(x: 8.85, y: 14))
+            bezierPath.addLine(to: CGPoint(x: 15, y: 7.64))
+            bezierPath.addLine(to: CGPoint(x: 14.23, y: 7))
+            bezierPath.addLine(to: CGPoint(x: 8.85, y: 12.73))
+            bezierPath.addLine(to: CGPoint(x: 5.77, y: 9.55))
+            bezierPath.addLine(to: CGPoint(x: 5, y: 10.18))
+            bezierPath.close()
+            UIColor.gray.setFill()
+            bezierPath.fill()
+            UIColor.black.setStroke()
+            bezierPath.lineWidth = 3
+            bezierPath.lineCapStyle = .round
+            bezierPath.lineJoinStyle = .round
+            bezierPath.stroke()
+            return bezierPath.cgPath
         }
     }
 }
