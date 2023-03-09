5883bd2f89b7b5862b10ecbf43574ef0640a8aaf
renanmassaroto <114941235+renanmassaroto@users.noreply.github.com>
2023-03-09 09:57:27 -0300

    feat: added attributed text to checkbox and radiobutton (#238)
    * feat: added attributed text to checkbox and radiobutton
    
    * feat: added attributed text example to RadioButton

--- OceanDesignSystem/Controllers/Components/CheckBoxViewController.swift
+++ OceanDesignSystem/Controllers/Components/CheckBoxViewController.swift
@@ -17,28 +17,49 @@ final public class CheckBoxViewController : UIViewController {
     private var ck3: Ocean.CheckBox!
     private var ck4: Ocean.CheckBox!
     private var ck5: Ocean.CheckBox!
+    private var ck6: Ocean.CheckBox!
     
     public override func viewDidLoad() {
         self.view.backgroundColor = .white
         
         ck1 = Ocean.CheckBox { ck in
-            ck.label = "Check Box 1"
+            ck.text = "Check Box 1"
         }
         ck2 = Ocean.CheckBox { ck in
-            ck.label = "Check Box 2 with large text and many words. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            ck.text = "Check Box 2 with large text and many words. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         }
         ck3 = Ocean.CheckBox { ck in
-            ck.label = "Check Box 3 Error"
+            ck.text = "Check Box 3 Error"
             ck.errorMessage = "Mensagem de erro"
         }
         ck4 = Ocean.CheckBox { ck in
-            ck.label = "Check Box 1 Disabled"
+            ck.text = "Check Box 1 Disabled"
             ck.isSelected = true
             ck.isEnabled = false
         }
         ck5 = Ocean.CheckBox { ck in
-            ck.label = "Check Box 2 Disabled"
+            ck.text = "Check Box 2 Disabled"
             ck.isEnabled = false
+        }
+        ck6 = Ocean.CheckBox { ck in
+            let link = "Blu"
+            let text = "Check Box with attributed text, so why not check \(link) out?"
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont(name: Ocean.font.fontFamilyBaseWeightMedium,
+                              size: Ocean.font.fontSizeXxxs)!,
+                .foregroundColor: Ocean.color.colorInterfaceDarkDown
+            ]
+            
+            let attrString = NSMutableAttributedString(string: text)
+            attrString.addAttributes(attributes, range: (text as NSString).range(of: text))
+            
+            attrString.addAttribute(.link,
+                                    value: "https://blu.com.br/",
+                                    range: (text as NSString).range(of: link))
+            
+            ck.attributedText = attrString
+            ck.isEnabled = true
         }
         
         let stack = Ocean.StackView()
@@ -53,6 +74,7 @@ final public class CheckBoxViewController : UIViewController {
         stack.addArrangedSubview(Ocean.Spacer(space: Ocean.size.spacingStackSm))
         stack.addArrangedSubview(ck4)
         stack.addArrangedSubview(ck5)
+        stack.addArrangedSubview(ck6)
         
         self.view.addSubview(stack)
 
--- OceanDesignSystem/Controllers/Components/RadioButtonViewController.swift
+++ OceanDesignSystem/Controllers/Components/RadioButtonViewController.swift
@@ -17,26 +17,27 @@ final public class RadioButtonViewController : UIViewController {
     private var rb3: Ocean.RadioButton!
     private var rb4: Ocean.RadioButton!
     private var rb5: Ocean.RadioButton!
+    private var rb6: Ocean.RadioButton!
     
     public override func viewDidLoad() {
         self.view.backgroundColor = .white
         
         rb1 = Ocean.RadioButton { rb in
-            rb.label = "Radio button 1"
+            rb.text = "Radio button 1"
             rb.onTouch = {
                 self.rb2.isSelected = false
                 self.rb3.isSelected = false
             }
         }
         rb2 = Ocean.RadioButton { rb in
-            rb.label = "Radio button 2 with large text and many words. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            rb.text = "Radio button 2 with large text and many words. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             rb.onTouch = {
                 self.rb1.isSelected = false
                 self.rb3.isSelected = false
             }
         }
         rb3 = Ocean.RadioButton { rb in
-            rb.label = "Radio button 3 Error"
+            rb.text = "Radio button 3 Error"
             rb.errorMessage = "Mensagem de erro"
             rb.onTouch = {
                 self.rb1.isSelected = false
@@ -44,13 +45,33 @@ final public class RadioButtonViewController : UIViewController {
             }
         }
         rb4 = Ocean.RadioButton { rb in
-            rb.label = "Radio button 1 Disabled"
+            rb.text = "Radio button 1 Disabled"
             rb.isSelected = true
             rb.isEnabled = false
         }
         rb5 = Ocean.RadioButton { rb in
-            rb.label = "Radio button 2 Disabled"
+            rb.text = "Radio button 2 Disabled"
             rb.isEnabled = false
+        }
+        rb6 = Ocean.RadioButton { ck in
+            let link = "Blu"
+            let text = "Radio button with attributed text, so why not check \(link) out?"
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont(name: Ocean.font.fontFamilyBaseWeightMedium,
+                              size: Ocean.font.fontSizeXxxs)!,
+                .foregroundColor: Ocean.color.colorInterfaceDarkDown
+            ]
+            
+            let attrString = NSMutableAttributedString(string: text)
+            attrString.addAttributes(attributes, range: (text as NSString).range(of: text))
+            
+            attrString.addAttribute(.link,
+                                    value: "https://blu.com.br/",
+                                    range: (text as NSString).range(of: link))
+            
+            ck.attributedText = attrString
+            ck.isEnabled = true
         }
         
         let stack = Ocean.StackView()
@@ -66,6 +87,7 @@ final public class RadioButtonViewController : UIViewController {
         stack.addArrangedSubview(Ocean.Spacer(space: Ocean.size.spacingStackSm))
         stack.addArrangedSubview(rb4)
         stack.addArrangedSubview(rb5)
+        stack.addArrangedSubview(rb6)
         
         self.view.addSubview(stack)
 
