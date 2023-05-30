0c429a3505e031f7cd875246efa9f33dfabe359b
renanmassaroto <114941235+renanmassaroto@users.noreply.github.com>
2023-05-30 21:01:20 +0000

    feat: add unordered type for OrderedListItem (#273)
    * feat: added OrderedListItem with unordered type
    
    * feat: changed skeleton

---
+++ OceanComponents/Classes/Components/OrderedList/Ocean+listItem.swift
@@ -0,0 +1,32 @@
+//
+//  Ocean+listItem.swift
+//  Charts
+//
+//  Created by Renan Massaroto on 30/05/23.
+//
+
+import OceanTokens
+import UIKit
+
+extension Ocean {
+    public typealias OrderedListItemBuilder = ((OrderedListItem) -> Void)?
+    
+    public struct ListItem {
+        public static func ordered(builder: OrderedListItemBuilder = nil) -> OrderedListItem {
+            let item = OrderedListItem(builder: builder)
+            item.roundedBackgroundColor = Ocean.color.colorInterfaceLightUp
+            item.roundedTintColor = Ocean.color.colorBrandPrimaryDown
+            
+            return item
+        }
+        
+        public static func unordered(builder: OrderedListItemBuilder = nil) -> OrderedListItem {
+            let item = OrderedListItem(builder: builder)
+            item.image = Ocean.icon.chevronRightSolid
+            item.roundedBackgroundColor = Ocean.color.colorStatusNegativeUp
+            item.roundedTintColor = Ocean.color.colorStatusNegativePure
+            
+            return item
+        }
+    }
+}
