e0f8aecef6da6dedacda27cc642b5603538c9490
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2023-07-17 17:42:41 -0300

    fix: change html to underline

--- OceanComponents/Classes/Components/Link/Ocean+Link.swift
+++ OceanComponents/Classes/Components/Link/Ocean+Link.swift
@@ -9,43 +9,43 @@ import Foundation
 import OceanTokens
 
 extension Ocean {
-    
+
     public class Link: UIView {
-        
+
         public enum Size {
             case medium, small
         }
-        
+
         public enum IconType {
             case none, chevron, external
         }
-        
+
         public var title: String? {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var size: Size? = .medium {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var attributedTitle: NSAttributedString? {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var iconType: IconType = .none {
             didSet {
                 updateUI()
             }
         }
-        
+
         public var onTouch: (() -> Void) = {  }
-        
+
         private lazy var linkTitleLabel: UILabel = {
             Ocean.Typography.heading5 { label in
                 label.translatesAutoresizingMaskIntoConstraints = false
@@ -53,73 +53,74 @@ extension Ocean {
                 label.textColor = Ocean.color.colorBrandPrimaryPure
             }
         }()
-        
+
         private lazy var linkIconImageView: UIImageView = {
             let imageView = UIImageView()
             imageView.translatesAutoresizingMaskIntoConstraints = false
             imageView.contentMode = .scaleAspectFit
             imageView.tintColor = Ocean.color.colorBrandPrimaryPure
-            
+
             return imageView
         }()
-        
+
         private lazy var mainStack: Ocean.StackView = {
             let stack = Ocean.StackView()
             stack.axis = .horizontal
             stack.distribution = .fill
             stack.alignment = .center
             stack.spacing = Ocean.size.spacingStackXxs
-            
+
             stack.add([
                 linkTitleLabel,
                 linkIconImageView
             ])
-            
+
             stack.addTapGesture(target: self, selector: #selector(onLinkTouch))
-            
+
             return stack
         }()
-        
+
         public override init(frame: CGRect = .zero) {
             super.init(frame: frame)
             setupUI()
         }
-        
+
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
-        
+
         private func setupUI() {
             addSubviews(mainStack)
             setupConstraints()
         }
-        
+
         private func setupConstraints() {
             mainStack.oceanConstraints
                 .fill(to: self)
                 .make()
-            
+
             linkIconImageView.oceanConstraints
                 .leadingToTrailing(to: linkTitleLabel)
                 .width(constant: 16)
                 .height(constant: 16)
                 .make()
         }
-        
+
         private func updateUI() {
             let size = size == .medium ? Ocean.font.fontSizeXs : Ocean.font.fontSizeXxs
-            
+
+            linkTitleLabel.font = .highlightBold(size: size)
+
             if let attributedTitle = attributedTitle {
                 linkTitleLabel.attributedText = attributedTitle
             } else if let title = title {
-                linkTitleLabel.attributedText = "<u>\(title)</u>".htmlToAttributedText(
-                    size: size,
-                    color: Ocean.color.colorBrandPrimaryPure
-                )
+                linkTitleLabel.underlineText(fullText: title,
+                                             underlinedText: title,
+                                             underlinedTextColor: Ocean.color.colorBrandPrimaryPure)
             }
             changeIcon()
         }
-        
+
         private func changeIcon() {
             let icon: UIImage?
             switch iconType {
@@ -133,7 +134,7 @@ extension Ocean {
             linkIconImageView.isHidden = icon == nil
             linkIconImageView.image = icon?.withRenderingMode(.alwaysTemplate)
         }
-        
+
         @objc
         private func onLinkTouch() {
             onTouch()
