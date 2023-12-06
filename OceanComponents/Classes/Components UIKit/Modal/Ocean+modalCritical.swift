44536e9f0316b93cbb059293546f170ec7247bf2
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2023-12-06 16:19:27 -0300

    feat: add shouldDismiss

--- OceanComponents/Classes/Components UIKit/Modal/Ocean+modalCritical.swift
+++ OceanComponents/Classes/Components UIKit/Modal/Ocean+modalCritical.swift
@@ -11,14 +11,19 @@ import OceanTokens
 extension Ocean {
     public class ModalCritical: Modal {
         public override func withAction(textNegative: String,
-                        actionNegative: (() -> Void)?,
-                        textPositive: String,
-                        actionPositive: (() -> Void)?) -> Modal {
+                                        actionNegative: (() -> Void)?,
+                                        textPositive: String,
+                                        actionPositive: (() -> Void)?,
+                                        shouldDismiss: Bool = true) -> Modal {
             modalViewController.actionsAxis = .horizontal
             modalViewController.actions.append(Ocean.Button.secondaryMD { button in
                 button.text = textNegative
                 button.onTouch = {
-                    self.modalViewController.dismiss(animated: true, wasClosed: false) {
+                    if shouldDismiss {
+                        self.modalViewController.dismiss(animated: true, wasClosed: false) {
+                            actionNegative?()
+                        }
+                    } else {
                         actionNegative?()
                     }
                 }
@@ -26,27 +31,37 @@ extension Ocean {
             modalViewController.actions.append(Ocean.Button.primaryCriticalMD { button in
                 button.text = textPositive
                 button.onTouch = {
-                    self.modalViewController.dismiss(animated: true, wasClosed: false) {
+                    if shouldDismiss {
+                        self.modalViewController.dismiss(animated: true, wasClosed: false) {
+                            actionPositive?()
+                        }
+                    } else {
                         actionPositive?()
                     }
                 }
             })
             return self
         }
-        
-        public override func withActionPrimary(text: String, action: (() -> Void)?) -> Modal {
+
+        public override func withActionPrimary(text: String,
+                                               action: (() -> Void)?,
+                                               shouldDismiss: Bool = true) -> Modal {
             modalViewController.actionsAxis = .vertical
             modalViewController.actions.append(Ocean.Button.primaryCriticalBlockedMD { button in
                 button.text = text
                 button.onTouch = {
-                    self.modalViewController.dismiss(animated: true, wasClosed: false) {
+                    if shouldDismiss {
+                        self.modalViewController.dismiss(animated: true, wasClosed: false) {
+                            action?()
+                        }
+                    } else {
                         action?()
                     }
                 }
             })
             return self
         }
-        
+
         public override func build() -> ModalViewController {
             self.modalViewController.contentIsCritical = true
             self.modalViewController.makeView()
