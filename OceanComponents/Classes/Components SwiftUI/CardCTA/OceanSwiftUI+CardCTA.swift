1490e38c6088fb477fa77d50a14dc7c0101ce72a
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2024-02-09 17:20:07 -0300

    Update OceanSwiftUI+CardCTA.swift

--- OceanComponents/Classes/Components SwiftUI/CardCTA/OceanSwiftUI+CardCTA.swift
+++ OceanComponents/Classes/Components SwiftUI/CardCTA/OceanSwiftUI+CardCTA.swift
@@ -60,7 +60,7 @@ extension OceanSwiftUI {
         // MARK: View SwiftUI
 
         public var body: some View {
-            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0) {
+            HStack(alignment: .center, spacing: 0) {
                 if self.parameters.isLoading {
                     Spacer()
 
