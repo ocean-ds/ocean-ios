04fd7e6a6920038921e0c9c6958954c5dde4f9f7
AcassioM <acassio.vilasboas@useblu.com.br>
2024-06-18 20:35:04 -0300

    feat: added new component in swiftUI SettingListItem

---
+++ OceanComponents/Classes/Components SwiftUI/SettingListItem/OceanSwiftUI+SettingListItem.swift
@@ -0,0 +1,183 @@
+//
+//  OceanSwiftUI+SettingListItem.swift
+//  DGCharts
+//
+//  Created by Acassio Mendonça on 18/06/24.
+//
+
+import Foundation
+import OceanTokens
+import SwiftUI
+
+extension OceanSwiftUI {
+
+    // MARK: Parameter
+
+    public class SettingListItemParameters: ObservableObject {
+        @Published public var title: String
+        @Published public var description: String
+        @Published public var caption: String
+        @Published public var hasDivider: Bool
+        @Published public var tagTitle: String
+        @Published public var tagStatus: TagParameters.Status
+        @Published public var type: SettingListItemType
+        @Published public var buttonTitle: String
+        public var buttonAction: () -> Void
+
+        public init(title: String = "",
+                    description: String = "",
+                    caption: String = "",
+                    hasDivider: Bool = true,
+                    tagTitle: String = "",
+                    tagStatus: TagParameters.Status = .warning,
+                    type: SettingListItemType = .unchangedPrimary,
+                    buttonTitle: String = "",
+                    buttonAction: @escaping () -> Void = { }) {
+            self.title = title
+            self.description = description
+            self.caption = caption
+            self.hasDivider = hasDivider
+            self.tagTitle = tagTitle
+            self.tagStatus = tagStatus
+            self.type = type
+            self.buttonTitle = buttonTitle
+            self.buttonAction = buttonAction
+        }
+
+        public enum SettingListItemType {
+            case unchangedPrimary
+            case unchangedSecondary
+            case unchangedBlocked
+            case pending
+            case changedPrimary
+            case changedSecondary
+            case changedBlocked
+        }
+    }
+
+    public struct SettingListItem: View {
+        // MARK: Properties for UIKit
+
+        public lazy var hostingController = UIHostingController(rootView: self)
+        public lazy var uiView = hostingController.getUIView()
+
+        // MARK: Builder
+
+        public typealias Builder = (SettingListItem) -> Void
+
+        // MARK: Properties
+
+        @ObservedObject public var parameters: SettingListItemParameters
+
+        // MARK: Private properties
+
+        @ViewBuilder
+        private var leadingView: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                if !parameters.title.isEmpty {
+                    Typography.description { label in
+                        label.parameters.text = parameters.title
+                    }
+                }
+
+                if !parameters.description.isEmpty {
+                    Typography.paragraph { label in
+                        label.parameters.text = parameters.description
+                        label.parameters.textColor = getDescriptionColor()
+                    }
+                }
+
+                if !parameters.caption.isEmpty {
+                    Spacer()
+                        .frame(height: Ocean.size.spacingStackXxs)
+
+                    Typography.caption { label in
+                        label.parameters.text = parameters.caption
+                    }
+                }
+            }
+        }
+
+        @ViewBuilder
+        private var trailingView: some View {
+            VStack(alignment: .trailing, spacing: 0) {
+                switch parameters.type {
+                case .unchangedPrimary, .unchangedSecondary, .changedPrimary, .changedSecondary:
+                    if !parameters.buttonTitle.isEmpty {
+                        Button { button in
+                            button.parameters.text = parameters.buttonTitle
+                            button.parameters.style = getButtonStyle()
+                            button.parameters.size = .small
+                            button.parameters.onTouch = parameters.buttonAction
+                        }
+                    }
+                case .unchangedBlocked, .changedBlocked:
+                    Image(uiImage: Ocean.icon.lockClosedSolid)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color(Ocean.color.colorInterfaceDarkUp))
+                        .frame(width: 20, height: 20)
+                default:
+                    if !parameters.tagTitle.isEmpty {
+                        Tag { tag in
+                            tag.parameters.label = parameters.tagTitle
+                            tag.parameters.status = parameters.tagStatus
+                        }
+                    }
+                }
+            }
+        }
+
+        // MARK: Constructors
+
+        public init(parameters: SettingListItemParameters = SettingListItemParameters()) {
+            self.parameters = parameters
+        }
+
+        public init(builder: Builder) {
+            self.init()
+            builder(self)
+        }
+
+        // MARK: View SwiftUI
+
+        public var body: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(alignment: .center, spacing: 0) {
+                    leadingView
+                        .padding()
+
+                    Spacer()
+
+                    trailingView
+                        .padding(.trailing)
+                        .fixedSize(horizontal: true, vertical: false)
+                }
+
+                if parameters.hasDivider {
+                    Divider()
+                }
+            }
+        }
+
+        // MARK: Private Methods
+
+        private func getDescriptionColor() -> UIColor {
+            switch parameters.type {
+            case .changedBlocked, .changedPrimary, .changedSecondary:
+                return Ocean.color.colorInterfaceDarkUp
+            default:
+                return Ocean.color.colorInterfaceDarkPure
+            }
+        }
+
+        private func getButtonStyle() -> ButtonParameters.Style {
+            switch parameters.type {
+            case .unchangedPrimary, .changedPrimary:
+                return .primary
+            default:
+                return .secondary
+            }
+        }
+    }
+}
---
+++ OceanDesignSystem/Controllers/Components SwiftUI/SettingListItemSwiftUIViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingListItem.swift
+//  OceanDesignSystem
+//
+//  Created by Acassio Mendonça on 18/06/24.
+//  Copyright © 2024 Blu Pagamentos. All rights reserved.
+//
+
+import Foundation
