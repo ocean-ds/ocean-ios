e509c9bb198c933afff45b0fdd7b1410b3ec3a7c
AcassioM <acassio.vilasboas@useblu.com.br>
2025-01-28 17:18:12 -0300

    feat(filter-bar): update exemple viewController

--- OceanDesignSystem/Controllers/Components SwiftUI/FilterBarSwiftUIViewController.swift
+++ OceanDesignSystem/Controllers/Components SwiftUI/FilterBarSwiftUIViewController.swift
@@ -10,59 +10,74 @@ import OceanTokens
 import SwiftUI
 
 class FilterBarSwiftUIViewController: UIViewController {
+    var beginDate: Date? = nil
+    var endDate: Date? = nil
 
-    lazy var filterBar1 = OceanSwiftUI.FilterBar { filterBar in
-        filterBar.parameters.rootViewController = self
-        filterBar.parameters.groups = [
-            .init(mode: .multiple, options: [
-                .init(title: "Filtro 1",
-                      mode: .multiple,
-                      chips: [
-                        .init(id: "paid1", number: 1, title: "Pago"),
-                        .init(id: "pending1", number: 2, title: "Pendente"),
-                        .init(id: "refused1", number: 3, title: "Recusado")]),
-                .init(title: "Filtro 2",
-                      mode: .multiple,
-                      chips: [.init(id: "paid1", title: "Pago")]),
-                .init(title: "Filtro 3",
-                      mode: .single,
-                      chips: [
-                        .init(id: "paid2", title: "Pago"),
-                        .init(id: "pending2", title: "Pendente"),
-                        .init(id: "refused2", title: "Recusado"),
-                      ])
-            ]),
-            .init(options: [
-                .init(leadingIcon: Ocean.icon.adjustmentsOutline,
-                      title: "Filtro 3",
-                      chips: [.init(id: "filter3", title: "Filtro 3")]),
-                .init(title: "Filtro 4",
-                      chips: [.init(id: "filter4", title: "Filtro 4")]),
-                .init(title: "Filtro 5",
-                      chips: [.init(id: "filter5", title: "Filtro 5")])
-            ])
-        ]
-        filterBar.parameters.onTouch = { [weak self] selectedChips, touchedOption in
-            guard let self = self else { return false }
+    private var filterBar1: OceanSwiftUI.FilterBar {
+        OceanSwiftUI.FilterBar { filterBar in
+            filterBar.parameters.rootViewController = self
+            filterBar.parameters.groups = [
+                .init(mode: .multiple, options: [
+                    .init(title: "Date Range",
+                          beginDate: beginDate,
+                          endDate: endDate),
+                    .init(title: "Filtro 1",
+                          mode: .multiple,
+                          chips: [
+                            .init(id: "paid1", title: "Pago"),
+                            .init(id: "pending1", title: "Pendente"),
+                            .init(id: "refused1", title: "Recusado")]),
+                    .init(title: "Filtro 2",
+                          mode: .multiple,
+                          chips: [.init(id: "paid1", title: "Pago")]),
+                    .init(title: "Filtro 3",
+                          mode: .single,
+                          chips: [
+                            .init(id: "paid2", title: "Pago"),
+                            .init(id: "pending2", title: "Pendente"),
+                            .init(id: "refused2", title: "Recusado"),
+                          ])
+                ]),
+                .init(options: [
+                    .init(leadingIcon: Ocean.icon.adjustmentsOutline,
+                          title: "Filtro 3",
+                          chips: [.init(id: "filter3", title: "Filtro 3")]),
+                    .init(title: "Filtro 4",
+                          chips: [.init(id: "filter4", title: "Filtro 4")]),
+                    .init(title: "Filtro 5",
+                          chips: [.init(id: "filter5", title: "Filtro 5")])
+                ])
+            ]
+            filterBar.parameters.onTouch = { [weak self] selectedChips, touchedOption in
+                guard let self = self else { return false }
 
-            if touchedOption.title == "Filtro 5" {
-                self.showSnackbar(text: "Touched \(touchedOption.title) with \(selectedChips.map { $0.title }.joined(separator: ", "))")
-                return true
+                if touchedOption.title == "Filtro 5" {
+                    self.showSnackbar(text: "Touched \(touchedOption.title) with \(selectedChips.map { $0.title }.joined(separator: ", "))")
+                    return true
+                }
+
+                return false
             }
+            filterBar.parameters.onSelectionChange = { [weak self] selectedChips, _ in
+                guard let self = self else { return }
 
-            return false
-        }
-        filterBar.parameters.onSelectionChange = { [weak self] selectedChips, _ in
-            guard let self = self else { return }
+                if selectedChips.count > 0 {
+                    self.showSnackbar(text: "All selected filters are \(selectedChips.map({ $0.title }).joined(separator: ", "))")
+                } else {
+                    self.showSnackbar(text: "No items are selected")
+                }
+            }
+            filterBar.parameters.onDateRangeChange = { [weak self] beginDate, endDate, _ in
+                guard let self = self else { return }
 
-            if selectedChips.count > 0 {
-                self.showSnackbar(text: "All selected filters are \(selectedChips.map({ $0.title }).joined(separator: ", "))")
-            } else {
-                self.showSnackbar(text: "No items are selected")
+                self.beginDate = beginDate
+                self.endDate = endDate
+                
+                self.showSnackbar(text: "Dates selected \(beginDate?.shortDateFormatss()) a \(endDate?.shortDateFormatss())")
             }
         }
     }
-    
+
     lazy var filterBar2 = OceanSwiftUI.FilterBar { filterBar in
         filterBar.parameters.rootViewController = self
         filterBar.parameters.options = [
@@ -70,12 +85,13 @@ class FilterBarSwiftUIViewController: UIViewController {
             .init(chips: [.init(number: 3, title: "Filtro 2")]),
             .init(chips: [.init(number: 4, title: "Filtro 3")])
         ]
-        
-        filterBar.parameters.onSelectionChange = { [weak self] items, _ in
-            guard let self = self, let selectedItem = items.first(where: { $0.isSelected }),
-                  let id = selectedItem.id else { return }
-            
-            self.showSnackbar(text: selectedItem.title)
+
+        filterBar.parameters.onTouch = { [weak self] _, selectedOption in
+            guard let self = self else { return false }
+
+            print(selectedOption)
+
+            return false
         }
     }
     
