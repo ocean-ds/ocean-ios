89fb6c9586cc1190494faa1a575760318091fcd5
AcassioM <acassio.vilasboas@useblu.com.br>
2025-01-28 17:17:32 -0300

    feat(modal-with-date-range): add modal date range builder

--- OceanComponents/Classes/Components UIKit/Modal/Ocean+modalDateRange.swift
+++ OceanComponents/Classes/Components UIKit/Modal/Ocean+modalDateRange.swift
@@ -1,1 +1,49 @@
+//
+//  Ocean+modalDateRange.swift
+//  OceanComponents
+//
+//  Created by Acassio Mendonça on 23/01/25.
+//
 
+import UIKit
+import OceanTokens
+
+extension Ocean {
+    public class ModalDateRange {
+        private var modalViewController: ModalDateRangeViewController
+
+        public init(_ rootViewController: UIViewController) {
+            modalViewController = ModalDateRangeViewController(rootViewController)
+        }
+
+        public func withDismiss(_ value: Bool) -> ModalDateRange {
+            modalViewController.swipeDismiss = value
+            return self
+        }
+
+        public func withTitle(_ title: String?) -> ModalDateRange {
+            modalViewController.contentTitle = title
+            return self
+        }
+
+        public func withBeginDate(_ date: Date?) -> ModalDateRange {
+            modalViewController.beginDate = date
+            return self
+        }
+
+        public func withEndDate(_ date: Date?) -> ModalDateRange {
+            modalViewController.endDate = date
+            return self
+        }
+
+        public func withCompletion(_ completion: @escaping (Date?, Date?) -> ()) -> ModalDateRange {
+            modalViewController.onFilter = completion
+            return self
+        }
+
+        public func build() -> ModalDateRangeViewController {
+            self.modalViewController.makeView()
+            return modalViewController
+        }
+    }
+}
