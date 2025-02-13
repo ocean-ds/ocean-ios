c3112e66a0400193f2410b8a90ad675db46eab1d
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2025-02-13 14:57:26 -0300

    hotfix/fix-spacing-expandableTextListItem (#555)
    * fix: spacing
    
    * fix: spacing vstack body
    
    * fix: padding bottom expanded

--- OceanComponents/Classes/Components SwiftUI/ExpandableTextListItem/OceanSwiftUI+ExpandableTextListItem.swift
+++ OceanComponents/Classes/Components SwiftUI/ExpandableTextListItem/OceanSwiftUI+ExpandableTextListItem.swift
@@ -92,7 +92,7 @@ extension OceanSwiftUI {
         // MARK: View SwiftUI
 
         public var body: some View {
-            VStack(alignment: .leading, spacing: Ocean.size.spacingStackXs) {
+            VStack(alignment: .leading, spacing: 0) {
                 HStack(spacing: Ocean.size.spacingStackXs) {
                     if let icon = parameters.icon {
                         Image(uiImage: icon)
@@ -143,9 +143,12 @@ extension OceanSwiftUI {
                 .animation(.easeInOut(duration: animationDuration), value: parameters.status)
 
                 if parameters.status == .expanded {
-                    ForEach(parameters.items.indices, id: \.self) { index in
-                        OceanSwiftUI.TextListItem(parameters: parameters.items[index])
+                    VStack(alignment: .leading, spacing: 0) {
+                        ForEach(parameters.items.indices, id: \.self) { index in
+                            OceanSwiftUI.TextListItem(parameters: parameters.items[index])
+                        }
                     }
+                    .padding(.bottom, Ocean.size.spacingStackXxs)
                 }
             }
         }
