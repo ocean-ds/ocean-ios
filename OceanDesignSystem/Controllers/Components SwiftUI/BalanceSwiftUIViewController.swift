afd7df6132d0c9724af696fe0f8f51f18cadf2db
Celso Farias <136832936+FariasBlu@users.noreply.github.com>
2025-04-01 17:37:02 -0300

    Feature/APP-7563 update balance component (#570)
    * feat: initial step
    
    * feat: second step
    
    * feat: convert group to folder
    
    * feat: add enum display mode
    
    * fix: space
    
    * feat: add logo of acquirers
    
    * feat: relation of icons added
    
    * feat: add logo cielo
    
    * feat: add logos acquirers
    
    * feat: add logo pagar me
    
    * feat: add logos
    
    * feat: call logos in the controller
    
    * feat: add balance with more three acquirers
    
    * feat: add icons in extension
    
    * feat: add style primaryInvertedWithSymbol badge
    
    * fix: code smell
    
    * feat: add limit show acquirers
    
    * fix: header file
    
    * feat: code review
    
    * fix: code review
    
    * fix: code review of Igão
    
    * fix: code review of Igão
    
    * fix: code review of Igão
    
    * fix: ocean constraints
    
    * feat: add animation
    
    * fix: code review acassio
    
    * feat: remove animation

--- OceanDesignSystem/Controllers/Components SwiftUI/BalanceSwiftUIViewController.swift
+++ OceanDesignSystem/Controllers/Components SwiftUI/BalanceSwiftUIViewController.swift
@@ -13,41 +13,200 @@ import SwiftUI
 class BalanceSwiftUIViewController: UIViewController {
 
     private var model = OceanSwiftUI.BalanceModel(title: "Saldo total na Blu",
-                                                  value: 100,
+                                                  value: 1000000.0,
                                                   item1Title: "Saldo atual",
-                                                  item1Value: 50,
+                                                  item1Value: 500000.0,
                                                   item2Title: "Agenda",
-                                                  item2Value: 50,
-                                                  description: "Confira tudo o que entrou e saiu da sua Conta Digital Blu",
-                                                  actionCTA: "Extrato",
+                                                  item2Value: 500000.0,
+                                                  item3Value: 1000000.0,
+                                                  description: "Saldo em outras maquininhas",
+                                                  pendingTitle: "Aguardando recebimento",
+                                                  pendingValue: 1000000.0,
+                                                  actionCTA: "Receber",
+                                                  acquires: ["acquirer-cielo", "acquirer-getnet", "acquirer-stone"],
+                                                  displayMode: .amountMachines,
                                                   action: {
         print("Extrato")
     })
 
-    lazy var balance: OceanSwiftUI.Balance = {
+    private var model2 = OceanSwiftUI.BalanceModel(title: "Saldo total na Blu",
+                                                   value: 1000000.0,
+                                                   item1Title: "Saldo atual",
+                                                   item1Value: 500000.0,
+                                                   item2Title: "Agenda",
+                                                   item2Value: 500000.0,
+                                                   item3Value: 1000000.0,
+                                                   description: "Saldo em outras maquininhas",
+                                                   pendingTitle: "Aguardando recebimento",
+                                                   pendingValue: 1000000.0,
+                                                   actionCTA: "Receber",
+                                                   displayMode: .awaitPayment,
+                                                   action: {
+        print("Extrato")
+    })
+
+    private var model3 = OceanSwiftUI.BalanceModel(title: "Saldo total na Blu",
+                                                   value: 1000000.0,
+                                                   item1Title: "Saldo atual",
+                                                   item1Value: 500000.0,
+                                                   item2Title: "Agenda",
+                                                   item2Value: 500000.0,
+                                                   item3Value: 1000000.0,
+                                                   description: "Saldo em outras maquininhas",
+                                                   pendingTitle: "Suas vendas valem muito mais com  a Blu",
+                                                   pendingValue: 1000000.0,
+                                                   actionCTA: "Saiba mais",
+                                                   displayMode: .knowMore,
+                                                   action: {
+        print("Extrato")
+    })
+
+    private var model4 = OceanSwiftUI.BalanceModel(title: "Saldo total na Blu",
+                                                   value: 1000000.0,
+                                                   item1Title: "Saldo atual",
+                                                   item1Value: 500000.0,
+                                                   item2Title: "Agenda",
+                                                   item2Value: 500000.0,
+                                                   item3Value: 1000000.0,
+                                                   description: "Saldo em outras maquininhas",
+                                                   pendingTitle: "Adicione saldo do seu jeito e economize mais!",
+                                                   pendingValue: 1000000.0,
+                                                   actionCTA: "Adicione saldo",
+                                                   displayMode: .addBalance,
+                                                   action: {
+        print("Extrato")
+    })
+
+    private var model5 = OceanSwiftUI.BalanceModel(title: "Saldo total na Blu",
+                                                  value: 1000000.0,
+                                                  item1Title: "Saldo atual",
+                                                  item1Value: 500000.0,
+                                                  item2Title: "Agenda",
+                                                  item2Value: 500000.0,
+                                                  item3Value: 1000000.0,
+                                                  description: "Saldo em outras maquininhas",
+                                                  pendingTitle: "Aguardando recebimento",
+                                                  pendingValue: 1000000.0,
+                                                  actionCTA: "Receber",
+                                                  acquires: ["acquirer-safra", "acquirer-mercado-pago", "acquirer-cielo", "acquirer-getnet", "acquirer-stone"],
+                                                  displayMode: .amountMachines,
+                                                  action: {
+        print("Extrato")
+    })
+
+    lazy var balance1: OceanSwiftUI.Balance = {
         OceanSwiftUI.Balance { balance in
             balance.parameters.model = model
         }
     }()
 
+    lazy var balance2: OceanSwiftUI.Balance = {
+        OceanSwiftUI.Balance { balance in
+            balance.parameters.model = model2
+        }
+    }()
+
+    lazy var balance3: OceanSwiftUI.Balance = {
+        OceanSwiftUI.Balance { balance in
+            balance.parameters.model = model3
+        }
+    }()
+
+    lazy var balance4: OceanSwiftUI.Balance = {
+        OceanSwiftUI.Balance { balance in
+            balance.parameters.model = model4
+        }
+    }()
+
+
+    lazy var balance5: OceanSwiftUI.Balance = {
+        OceanSwiftUI.Balance { balance in
+            balance.parameters.model = model5
+        }
+    }()
+
+    lazy var acquirerAsaas = Image(uiImage: Ocean.icon.acquirerAsaas!)
+    lazy var acquirerCielo = Image(uiImage: Ocean.icon.acquirerCielo!)
+    lazy var acquirerGetnet = Image(uiImage: Ocean.icon.acquirerGetnet!)
+    lazy var acquirerInfinitePay = Image(uiImage: Ocean.icon.acquirerInfinitePay!)
+    lazy var acquirerMercadoPago = Image(uiImage: Ocean.icon.acquirerMercadoPago!)
+    lazy var acquirerPagBank = Image(uiImage: Ocean.icon.acquirerPagBank!)
+    lazy var acquirerPagarMe = Image(uiImage: Ocean.icon.acquirerPagarMe!)
+    lazy var acquirerRede = Image(uiImage: Ocean.icon.acquirerRede!)
+    lazy var acquirerSafra = Image(uiImage: Ocean.icon.acquirerSafra!)
+    lazy var acquirerSicoob = Image(uiImage: Ocean.icon.acquirerSicoob!)
+    lazy var acquirerStone = Image(uiImage: Ocean.icon.acquirerStone!)
+    lazy var acquirerSumup = Image(uiImage: Ocean.icon.acquirerSumup!)
+
     lazy var toogleButton: OceanSwiftUI.Button = {
         OceanSwiftUI.Button.secondarySM { button in
             button.parameters.text = "Toggle Scroll"
             button.parameters.onTouch = {
-                if self.balance.parameters.state == .scroll {
-                    self.balance.parameters.state = .collapsed
+                if (self.balance1.parameters.state == .scroll &&
+                    self.balance2.parameters.state == .scroll &&
+                    self.balance3.parameters.state == .scroll &&
+                    self.balance4.parameters.state == .scroll &&
+                    self.balance5.parameters.state == .scroll) {
+                    self.balance1.parameters.state = .collapsed
+                    self.balance2.parameters.state = .collapsed
+                    self.balance3.parameters.state = .collapsed
+                    self.balance4.parameters.state = .collapsed
+                    self.balance5.parameters.state = .collapsed
                 } else {
-                    self.balance.parameters.state = .scroll
+                    self.balance1.parameters.state = .scroll
+                    self.balance2.parameters.state = .scroll
+                    self.balance3.parameters.state = .scroll
+                    self.balance4.parameters.state = .scroll
+                    self.balance5.parameters.state = .scroll
                 }
             }
         }
     }()
 
-    public lazy var hostingController = UIHostingController(rootView: ScrollView {
-        VStack(spacing: Ocean.size.spacingStackMd) {
-            balance
+    lazy var showBalance: OceanSwiftUI.Button = {
+        OceanSwiftUI.Button.secondarySM { button in
+            button.parameters.text = self.balance1.parameters.isVisibleBalance ? "Esconder Saldo" : "Mostrar Saldo"
+            button.parameters.onTouch = {
+                self.balance1.parameters.isVisibleBalance.toggle()
+                self.balance2.parameters.isVisibleBalance.toggle()
+                self.balance3.parameters.isVisibleBalance.toggle()
+                self.balance4.parameters.isVisibleBalance.toggle()
+                self.balance5.parameters.isVisibleBalance.toggle()
+            }
+        }
+    }()
 
+    public lazy var hostingController = UIHostingController(rootView: ScrollView {
+        VStack(spacing: Ocean.size.spacingStackXs) {
+            balance1
+
+            balance5
+
+            balance2
+
+            balance3
+
+            balance4
+        }
+
+        HStack {
             toogleButton
+            showBalance
+        }
+
+        HStack(spacing: Ocean.size.spacingStackXxxs) {
+            acquirerAsaas
+            acquirerCielo
+            acquirerGetnet
+            acquirerInfinitePay
+            acquirerMercadoPago
+            acquirerPagBank
+            acquirerPagarMe
+            acquirerRede
+            acquirerSafra
+            acquirerSicoob
+            acquirerStone
+            acquirerSumup
         }
     })
 
@@ -75,4 +234,3 @@ struct BalanceSwiftUIViewController_Preview: PreviewProvider {
         }
     }
 }
-
