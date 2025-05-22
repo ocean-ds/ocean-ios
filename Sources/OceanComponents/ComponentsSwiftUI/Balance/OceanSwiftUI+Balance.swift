8d14c7799ae80e673d1b068a97eb88c7c2643bac
Celso Farias <136832936+FariasBlu@users.noreply.github.com>
2025-05-22 16:04:46 -0300

    Feature/APP-7551 Set first word in the place image acquirer (#584)
    * feat: set first word in the place image acquirer
    
    * feat: code review

--- Sources/OceanComponents/ComponentsSwiftUI/Balance/OceanSwiftUI+Balance.swift
+++ Sources/OceanComponents/ComponentsSwiftUI/Balance/OceanSwiftUI+Balance.swift
@@ -259,10 +259,17 @@ extension OceanSwiftUI {
                             .fill(Color.white)
                             .frame(width: 24, height: 24)
 
-                        Image(uiImage: acquirers[index].toOceanIcon() ?? UIImage())
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 22, height: 22)
+                        if let icon = acquirers[index].toOceanIcon() {
+                            Image(uiImage: icon)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 22, height: 22)
+                        } else {
+                            OceanSwiftUI.Typography.eyebrow { label in
+                                label.parameters.text = acquirers[index].prefix(1).uppercased()
+                                label.parameters.textColor = Ocean.color.colorBrandPrimaryDown
+                            }
+                        }
                     }
                 }
 
