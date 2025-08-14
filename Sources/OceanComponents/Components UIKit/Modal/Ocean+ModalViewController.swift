e4d0ac7b66146ff65fbc4922cea86f828454b834
Celso Farias <celso.farias@useblu.com.br>
2025-08-14 13:51:12 -0300

    fix: approach 9

--- Sources/OceanComponents/Components UIKit/Modal/Ocean+ModalViewController.swift
+++ Sources/OceanComponents/Components UIKit/Modal/Ocean+ModalViewController.swift
@@ -205,7 +205,7 @@ extension Ocean {
             let topSpacing = hasTopNotch ? 25.0 : 10.0
 
             mainStack.oceanConstraints
-                .topToTop(to: view, constant: 16.0, safeArea: true)
+                .topToTop(to: view, safeArea: true)
                 .centerX(to: view)
                 .width(constant: UIScreen.main.bounds.width - Ocean.size.spacingStackSm * 2)
                 .make()
