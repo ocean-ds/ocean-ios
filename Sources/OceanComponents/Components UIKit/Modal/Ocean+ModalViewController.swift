c0b3ada301eb5d1490de2853e34e38e4e4ac7c16
Celso Farias <celso.farias@useblu.com.br>
2025-08-14 12:22:01 -0300

    fix: approach 7 - adjusts spacings

--- Sources/OceanComponents/Components UIKit/Modal/Ocean+ModalViewController.swift
+++ Sources/OceanComponents/Components UIKit/Modal/Ocean+ModalViewController.swift
@@ -202,7 +202,7 @@ extension Ocean {
         }
 
         private func setupConstraints() {
-            let topSpacing = hasTopNotch ? 20.0 : 10.0
+            let topSpacing = hasTopNotch ? 25.0 : 10.0
 
             mainStack.oceanConstraints
                 .topToTop(to: view, constant: topSpacing)
