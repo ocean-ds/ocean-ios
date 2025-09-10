351753fb4386e4674a2dd0b5e6ed245ca104daf9
Celso Farias <136832936+FariasBlu@users.noreply.github.com>
2025-09-10 13:41:54 -0300

    feat: fix certificates (#597)

--- OceanDesignSystem/Controllers/Components SwiftUI/CardListItemSwiftUIViewController.swift
+++ OceanDesignSystem/Controllers/Components SwiftUI/CardListItemSwiftUIViewController.swift
@@ -125,7 +125,7 @@ class CardListItemSwiftUIViewController: UIViewController {
         view.parameters.title = "Crédito parcelado"
         view.parameters.titleColor = Ocean.color.colorInterfaceDarkDeep
         view.parameters.subtitle = "Parcele tudo em até 12 vezes com a 1ª parcela para 30 dias"
-        view.parameters.caption = "Crédito aprovado: R$ 9.000,00"
+        view.parameters.caption = "Crédito aprovado: R$ 10.000,00"
         view.parameters.captionColor = Ocean.color.colorBrandPrimaryPure
         view.parameters.highlightCaption = "Melhor escolha para o caixa da sua loja."
         view.parameters.hasRadioButton = true
