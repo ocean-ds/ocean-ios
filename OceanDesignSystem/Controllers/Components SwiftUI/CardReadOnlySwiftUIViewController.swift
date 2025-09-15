aa6739687c31ac0c28c0c2c15a6a98b820998dbe
Celso Farias <celso.farias@useblu.com.br>
2025-09-15 18:33:26 -0300

    fix: code review

--- OceanDesignSystem/Controllers/Components SwiftUI/CardReadOnlySwiftUIViewController.swift
+++ OceanDesignSystem/Controllers/Components SwiftUI/CardReadOnlySwiftUIViewController.swift
@@ -12,60 +12,58 @@ import SwiftUI
 class CardReadOnlySwiftUIViewController: UIViewController {
     lazy var view0: OceanSwiftUI.CardReadOnly = {
         return OceanSwiftUI.CardReadOnly { view in
-            view.parameters.title = "Parcelamento"
-            view.parameters.description = "R$ 2.991,52"
-            view.parameters.descriptionColor = Ocean.color.colorInterfaceDarkDeep
-            view.parameters.caption = "Crédito restante: R$ 6.008,48"
-            view.parameters.captionColor = Ocean.color.colorBrandPrimaryPure
-            view.parameters.type = .inverted
+            view.parameters.contentList.title = "Parcelamento"
+            view.parameters.contentList.description = "R$ 2.991,52"
+            view.parameters.contentList.descriptionColor = Ocean.color.colorInterfaceDarkDeep
+            view.parameters.contentList.caption = "Crédito restante: R$ 6.008,48"
+            view.parameters.contentList.captionColor = Ocean.color.colorBrandPrimaryPure
+            view.parameters.contentList.type = .inverted
         }
     }()
 
     lazy var view1: OceanSwiftUI.CardReadOnly = {
         return OceanSwiftUI.CardReadOnly { view in
-            view.parameters.title = "Title"
-            view.parameters.description = "Description"
-            view.parameters.type = .default
+            view.parameters.contentList.title = "Title"
+            view.parameters.contentList.description = "Description"
+            view.parameters.contentList.type = .default
         }
     }()
-
 
     lazy var view2: OceanSwiftUI.CardReadOnly = {
         return OceanSwiftUI.CardReadOnly { view in
-            view.parameters.title = "Title Inverted"
-            view.parameters.description = "Description"
-            view.parameters.type = .inverted
+            view.parameters.contentList.title = "Title Inverted"
+            view.parameters.contentList.description = "Description"
+            view.parameters.contentList.type = .inverted
         }
     }()
 
-
     lazy var view3: OceanSwiftUI.CardReadOnly = {
         return OceanSwiftUI.CardReadOnly { view in
-            view.parameters.title = "Title"
-            view.parameters.description = "Description"
-            view.parameters.caption = "Caption"
-            view.parameters.type = .default
+            view.parameters.contentList.title = "Title"
+            view.parameters.contentList.description = "Description"
+            view.parameters.contentList.caption = "Caption"
+            view.parameters.contentList.type = .default
         }
     }()
 
     lazy var view4: OceanSwiftUI.CardReadOnly = {
         return OceanSwiftUI.CardReadOnly { view in
-            view.parameters.title = "Title"
-            view.parameters.description = "Description"
-            view.parameters.caption = "Caption"
-            view.parameters.errorMessage = "Error message"
-            view.parameters.type = .default
+            view.parameters.contentList.title = "Title"
+            view.parameters.contentList.description = "Description"
+            view.parameters.contentList.caption = "Caption"
+            view.parameters.contentList.errorMessage = "Error message"
+            view.parameters.contentList.type = .default
         }
     }()
 
     lazy var view5: OceanSwiftUI.CardReadOnly = {
         return OceanSwiftUI.CardReadOnly { view in
-            view.parameters.title = "Title"
-            view.parameters.description = "Description"
-            view.parameters.caption = "Caption"
-            view.parameters.errorMessage = "Error message"
-            view.parameters.type = .default
-            view.parameters.showSkeleton = true
+            view.parameters.contentList.title = "Title"
+            view.parameters.contentList.description = "Description"
+            view.parameters.contentList.caption = "Caption"
+            view.parameters.contentList.errorMessage = "Error message"
+            view.parameters.contentList.type = .default
+            view.parameters.contentList.showSkeleton = true
         }
     }()
 
@@ -104,4 +102,3 @@ struct CardReadOnlySwiftUIViewController_Preview: PreviewProvider {
         }
     }
 }
-
--- Sources/OceanComponents/ComponentsSwiftUI/CardReadOnly/OceanSwiftUI+CardReadOnly.swift
+++ Sources/OceanComponents/ComponentsSwiftUI/CardReadOnly/OceanSwiftUI+CardReadOnly.swift
@@ -13,63 +13,24 @@ extension OceanSwiftUI {
     // MARK: Parameter
 
     public class CardReadOnlyParameters: ObservableObject {
-        @Published public var title: String
-        @Published public var titleColor: UIColor
-        @Published public var description: String
-        @Published public var descriptionColor: UIColor?
-        @Published public var descriptionFont: UIFont?
-        @Published public var newDescription: String
-        @Published public var caption: String
-        @Published public var captionColor: UIColor
-        @Published public var tagTitle: String
-        @Published public var tagStatus: TagParameters.Status
-        @Published public var errorMessage: String
-        @Published public var type: ContentListParameters.ContentListItemType
-        @Published public var showSkeleton: Bool
+        @Published public var contentList: OceanSwiftUI.ContentListParameters
         @Published public var backgroundColor: UIColor
         @Published public var cornerRadius: CGFloat
         @Published public var borderColor: UIColor?
         @Published public var borderWidth: CGFloat
-        @Published public var contentPadding: EdgeInsets
         @Published public var cardPadding: EdgeInsets
 
-        public init(title: String = "",
-                    description: String = "",
-                    titleColor: UIColor = Ocean.color.colorInterfaceDarkDown,
-                    descriptionColor: UIColor? = nil,
-                    descriptionFont: UIFont? = nil,
-                    newDescription: String = "",
-                    caption: String = "",
-                    captionColor: UIColor = Ocean.color.colorInterfaceDarkDown,
-                    tagTitle: String = "",
-                    tagStatus: TagParameters.Status = .warning,
-                    errorMessage: String = "",
-                    type: ContentListParameters.ContentListItemType = .default,
-                    showSkeleton: Bool = false,
+        public init(contentList: OceanSwiftUI.ContentListParameters = OceanSwiftUI.ContentListParameters(),
                     backgroundColor: UIColor = Ocean.color.colorInterfaceLightPure,
                     cornerRadius: CGFloat = Ocean.size.borderRadiusMd,
                     borderColor: UIColor? = nil,
                     borderWidth: CGFloat = Ocean.size.borderWidthHairline,
-                    contentPadding: EdgeInsets = .all(Ocean.size.spacingStackXs),
                     cardPadding: EdgeInsets = .all(0)) {
-            self.title = title
-            self.titleColor = titleColor
-            self.description = description
-            self.descriptionColor = descriptionColor
-            self.descriptionFont = descriptionFont
-            self.newDescription = newDescription
-            self.caption = caption
-            self.captionColor = captionColor
-            self.tagTitle = tagTitle
-            self.tagStatus = tagStatus
-            self.errorMessage = errorMessage
-            self.type = type
-            self.showSkeleton = showSkeleton
+            self.contentList = contentList
             self.backgroundColor = backgroundColor
             self.cornerRadius = cornerRadius
             self.borderColor = borderColor
             self.borderWidth = borderWidth
-            self.contentPadding = contentPadding
             self.cardPadding = cardPadding
         }
     }
@@ -106,21 +67,7 @@ extension OceanSwiftUI {
 
         public var body: some View {
             VStack(spacing: 0) {
-                ContentList { contentList in
-                    contentList.parameters.title = parameters.title
-                    contentList.parameters.description = parameters.description
-                    contentList.parameters.descriptionColor = parameters.descriptionColor
-                    contentList.parameters.descriptionFont = parameters.descriptionFont
-                    contentList.parameters.newDescription = parameters.newDescription
-                    contentList.parameters.caption = parameters.caption
-                    contentList.parameters.captionColor = parameters.captionColor
-                    contentList.parameters.tagTitle = parameters.tagTitle
-                    contentList.parameters.tagStatus = parameters.tagStatus
-                    contentList.parameters.errorMessage = parameters.errorMessage
-                    contentList.parameters.type = parameters.type
-                    contentList.parameters.showSkeleton = parameters.showSkeleton
-                    contentList.parameters.padding = parameters.contentPadding
-                }
+                ContentList(parameters: parameters.contentList)
             }
             .background(Color(parameters.backgroundColor))
             .border(cornerRadius: parameters.cornerRadius,
