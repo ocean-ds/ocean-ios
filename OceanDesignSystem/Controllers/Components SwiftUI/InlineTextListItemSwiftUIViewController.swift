5203a5e540194516e6262c35f3caa097306d3c6a
Celso Farias <136832936+FariasBlu@users.noreply.github.com>
2025-10-08 09:47:30 -0300

    Feature/Add ArrowUturnLefOutline + LinkAltOutline (#606)
    * feat: add arrow-u-turn-lef-outline
    
    * feat: add linkAltOutline

--- OceanDesignSystem/Controllers/Components SwiftUI/InlineTextListItemSwiftUIViewController.swift
+++ OceanDesignSystem/Controllers/Components SwiftUI/InlineTextListItemSwiftUIViewController.swift
@@ -11,6 +11,40 @@ import SwiftUI
 
 class InlineTextListItemSwiftUIViewController: UIViewController {
 
+    lazy var inlineTextListItem0: OceanSwiftUI.InlineTextListItem = {
+        let item = OceanSwiftUI.InlineTextListItemParameters.ItemModel(
+            text: "Teste",
+            value: "Ícone: arrow-u-turn-lef-outline",
+            valueColor: Ocean.color.colorStatusPositiveDeep
+        )
+
+        return OceanSwiftUI.InlineTextListItem { textListItem in
+            textListItem.parameters.item = item
+            textListItem.parameters.state = .positive
+            textListItem.parameters.size = .normal
+            textListItem.parameters.icon = .init(icon: Ocean.icon.arrowUturnLeftOutline!,
+                                                 color: Ocean.color.colorStatusPositiveDeep,
+                                                 backgroundColor: Ocean.color.colorInterfaceLightPure)
+        }
+    }()
+
+    lazy var inlineTextListItem01: OceanSwiftUI.InlineTextListItem = {
+        let item = OceanSwiftUI.InlineTextListItemParameters.ItemModel(
+            text: "Teste",
+            value: "Ícone: link-alt-outline",
+            valueColor: Ocean.color.colorStatusPositiveDeep
+        )
+
+        return OceanSwiftUI.InlineTextListItem { textListItem in
+            textListItem.parameters.item = item
+            textListItem.parameters.state = .positive
+            textListItem.parameters.size = .normal
+            textListItem.parameters.icon = .init(icon: Ocean.icon.linkAltOutline!,
+                                                 color: Ocean.color.colorStatusPositiveDeep,
+                                                 backgroundColor: Ocean.color.colorInterfaceLightPure)
+        }
+    }()
+
     lazy var inlineTextListItem1: OceanSwiftUI.InlineTextListItem = {
         let item = OceanSwiftUI.InlineTextListItemParameters.ItemModel(
             text: "Title",
@@ -550,6 +584,8 @@ class InlineTextListItemSwiftUIViewController: UIViewController {
 
     public lazy var hostingController = UIHostingController(rootView: ScrollView {
         VStack(spacing: Ocean.size.spacingStackXs) {
+            inlineTextListItem0
+            inlineTextListItem01
             inlineTextListItem1
             inlineTextListItem2
             inlineTextListItem3
